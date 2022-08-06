28e94a1b67e5052d2eb3fcfaedb293f86d5ef246
keaunsolNa <knsol1992@naver.com>
2022-08-06 19:51:57 +0900

    22.08.06 코딩 테스트

---
+++ CodingTest/src/codingTest/Lessons4.java
@@ -0,0 +1,54 @@
+package codingTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+class Lessons4 {
+	
+	public static void main(String[] args) {
+	
+		Lessons4 lessons4 = new Lessons4();
+		
+		String[] participant = {"mislav", "stanko", "mislav", "ana", "asdasd", "qweqwe", "cvxckvmkasd"};
+		String[] completion = {"stanko", "ana", "mislav", "asdasd", "qweqwe", "cvxckvmkasd"};
+		
+		String answer = lessons4.solution(participant, completion);
+		
+		System.out.println(answer);
+		
+	}
+    public String solution(String[] participant, String[] completion) {
+
+    	
+    	List<String> participantArrayList = new ArrayList<>(List.of(participant));
+    	List<String> completionArrayList = new ArrayList<>(List.of(completion));
+    	
+    	Collections.sort(participantArrayList);
+    	Collections.sort(completionArrayList);
+    	
+    	for(Iterator<String> iter=participantArrayList.iterator(); iter.hasNext();) {
+    		String str = iter.next();
+    		for(Iterator<String> iter2=completionArrayList.iterator(); iter2.hasNext();) {
+    			String str2 = iter2.next();
+    			
+    			if(str.equals(str2)) {
+    				
+    				iter.remove();
+    				iter2.remove();
+    				
+    				break;
+    			}
+    		}
+    	}
+    	
+    	String answer ="";
+    	for (String item:participantArrayList) {
+    		answer += item;
+    	}
+    	
+        System.out.println("ANSWER : " + answer);
+        return answer;
+    }
+}
