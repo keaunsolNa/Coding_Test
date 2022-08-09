5db550ea264f1cd1c4f804c6713519e1433a2e0a
keaunsolNa <knsol1992@naver.com>
2022-08-09 23:34:48 +0900

    2022.08.09 코딩 테스트

---
+++ CodingTest/src/codingTest/Lessons42.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Lessons42 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Lessons42 lessons42 = new Lessons42();
+		String result = lessons42.solution(5, 24);
+		
+		System.out.println(result);
+		
+	}
+	
+    public String solution(int a, int b) throws ParseException {
+    	
+//    	2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 
+//    	두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 
+//    	요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT입니다. 
+//    	예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+    	
+    	String date = "2016-"+a+"-"+b;
+    	
+		Date formatDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+		String answer = new SimpleDateFormat("E", new Locale("en", "US")).format(formatDate);
+		
+        return answer.toUpperCase();
+    }
+}
---
+++ CodingTest/src/codingTest/Lessons49.java
@@ -0,0 +1,52 @@
+package codingTest;
+
+public class Lessons49 {
+
+	public static void main(String[] args) {
+		
+		Lessons49 lessons49 = new Lessons49();
+		String s = "AaZz";
+		int n = 25;
+		String result = lessons49.solution(s, n);
+		
+		System.out.println("result : " + result);
+		
+	}
+	
+    public String solution(String s, int n) {
+    	
+//    	어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 
+//    	예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. 
+//    	"z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+    	
+    	char[] charArr = s.toCharArray();
+    	int newS;
+    	String answer = "";
+    	
+    	for(int i = 0; i < charArr.length; i++) {
+    		int charArrToInt = Integer.valueOf(charArr[i]);
+    		
+    		if(charArrToInt == 32) {
+    			answer += " ";
+    			continue;
+    		} 
+    		if( n > 24) {
+    			newS = charArrToInt + (n/24);
+    		}
+    			newS = charArrToInt + n;
+    		if(charArrToInt >= 97 && newS > 122) {
+    			int a = newS - 122;
+    			newS = 96 + a;
+    			answer += (char)newS;
+    		} else if(charArrToInt <= 90 && newS > 90) {
+    			int a = newS - 90;
+    			newS = 64 + a;
+    			answer += (char)newS;
+    		} else {
+    			answer += (char)newS;
+    		}
+    	}
+    	
+        return answer;
+    }
+}
