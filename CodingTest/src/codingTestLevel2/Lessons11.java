12351d0a928df21bab158f67d65b1e83da5ae862
keaunsolNa <knsol1992@naver.com>
2022-08-15 20:59:05 +0900

    2022.08.15 코딩 테스트

---
+++ CodingTest/src/codingTestLevel2/Lessons11.java
@@ -0,0 +1,53 @@
+package codingTestLevel2;
+
+import java.util.Stack;
+
+public class Lessons11 {
+
+	public static void main(String[] args) {
+
+		Lessons11 lessons11 = new Lessons11();
+		int[] arr = {2,6,8,14}; 
+		
+		int answer = lessons11.solution(arr);
+		
+		System.out.println("RESULT : " + answer);
+	}
+
+	public int solution(int[] arr) {
+		
+//		두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다. 
+//		예를 들어 2와 7의 최소공배수는 14가 됩니다. 
+//		정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. 
+//		n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+		
+		Stack<Integer> arrList = new Stack<>();
+		for (int i : arr) {
+			arrList.push(i);
+		}
+		
+		while(arrList.size() != 1) {
+			int lcm = lcm(arrList.pop(), arrList.pop());
+			System.out.println("lcm : " + lcm);
+			arrList.push(lcm);
+		}
+		
+        int answer = arrList.pop();
+        return answer;
+    }
+	
+	public static int gcd(int a, int b) {
+		
+		if(a%b == 0) {
+			System.out.println("gcd : " + b);
+			return b;
+		}
+		
+		return gcd(b, a%b);
+	}
+	
+	public static int lcm(int a, int b) {
+		
+		return a * b / gcd(a,b);
+	}
+}
