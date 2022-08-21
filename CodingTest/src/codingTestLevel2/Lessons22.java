ebf89152f2e2cd73ced3ec59cf277b732fb3b3f8
keaunsolNa <knsol1992@naver.com>
2022-08-22 07:42:16 +0900

    2022.08.22 코딩 테스

--- CodingTest/src/codingTestLevel2/Lessons22.java
+++ CodingTest/src/codingTestLevel2/Lessons22.java
@@ -1,7 +1,6 @@
 package codingTestLevel2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Lessons22 {
 	
@@ -9,7 +8,6 @@ public class Lessons22 {
 
 		Lessons22 lessons22 = new Lessons22();
 		
-		int n = 2;
 		String[] phone_book = {"119", "97674223", "1195524421"};
 		
 		boolean answer = lessons22.solution(phone_book);
@@ -19,7 +17,37 @@ public class Lessons22 {
 	}
 
 	public boolean solution(String[] phone_book) {
+		
+//		전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+//		전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+//
+//		구조대 : 119
+//		박준영 : 97 674 223
+//		지영석 : 11 9552 4421
+//		전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 
+//		어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+				
+				
         boolean answer = true;
+        
+        if(phone_book.length == 1) {
+        	return true;
+        }
+        
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length; i++) {
+        	if(i > phone_book.length - 1) {
+        		break;
+        	}
+        	System.out.println("NODE : " + phone_book[i+1]);
+        	System.out.println("COMPARE : " + phone_book[i]);
+        	if(phone_book[i+1].startsWith(phone_book[i])) {
+        		return false;
+        	}
+        }
+        
+        
         return answer;
     }
 	
