cec3a851b2c9e1307327e99880cb5ae91d055da1
keaunsolNa <knsol1992@naver.com>
2022-08-23 11:05:24 +0900

    2022.08.23 코딩 테스트

--- CodingTest/src/codingTestLevel2/Lessons22.java
+++ CodingTest/src/codingTestLevel2/Lessons22.java
@@ -37,7 +37,7 @@ public class Lessons22 {
         Arrays.sort(phone_book);
         
         for(int i = 0; i < phone_book.length; i++) {
-        	if(i > phone_book.length - 1) {
+        	if(i == phone_book.length - 1) {
         		break;
         	}
         	System.out.println("NODE : " + phone_book[i+1]);
