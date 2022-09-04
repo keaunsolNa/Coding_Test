69843bcadaa67ab83ed6e930b69a4785c3d0d899
keaunsolNa <knsol1992@naver.com>
2022-09-04 23:34:36 +0900

    2022.09.04 코딩 테스트

--- CodingTest/src/codingTestLevel2/Lessons33.java
+++ CodingTest/src/codingTestLevel2/Lessons33.java
@@ -8,7 +8,7 @@ public class Lessons33 {
 	public static void main(String[] args) {
 
 		Lessons33 lessons33 = new Lessons33();
-		int answer = lessons33.solution("1122125");
+		int answer = lessons33.solution("213");
 
 		System.out.println("Answer : " + answer);
 		
@@ -19,6 +19,13 @@ public class Lessons33 {
     Set<Integer> set = new HashSet<>();
 
     public int solution(String numbers) {
+    	
+//    	한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 
+//    	흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
+//
+//    	각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 
+//    	종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
+    			
         visit = new boolean[numbers.length()];
         arr = new char[numbers.length()];
 
@@ -44,6 +51,7 @@ public class Lessons33 {
 
         for(int i = 0; i < length; i++) {
             sb.append(arr[i]);
+            System.out.println("SB : " + sb);
         }
         
         return Integer.parseInt(sb.toString());
