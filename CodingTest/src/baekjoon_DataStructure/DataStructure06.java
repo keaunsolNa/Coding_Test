af30a7dc7e43aaf64664037396d7c4671460757c
keaunsolNa <knsol1992@naver.com>
2022-12-06 23:50:56 +0900

    2022.12.06 자료구조 코딩테스트

--- CodingTest/src/baekjoon_DataStructure/DataStructure06.java
+++ CodingTest/src/baekjoon_DataStructure/DataStructure06.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class DataStructure06 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		test02();
+		test05();
 	}
 	
 	// 절댓값 힙
@@ -101,28 +104,147 @@ public class DataStructure06 {
 		System.out.print(sb2);
 	}
 
-	// 야근
+	// Morse 
 	public static void test03() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb;
+		Map<String, String> morseMap = new HashMap<>();
+		for(int i = 0; i < 26; i++) {
+			st = new StringTokenizer(br.readLine());
+			String alp = st.nextToken();
+			String morse = st.nextToken();
+			
+			morseMap.put(alp, morse);
+		}
+		
+		int morseWordT = Integer.parseInt(br.readLine());
+		Map<String, String> dictionaryMap = new HashMap<>(); 
+		
+		for(int i = 0; i < morseWordT; i++) {
+			String temp = br.readLine();
+			sb = new StringBuilder();
+			for(int j = 0; j < temp.length(); j++) {
+				sb.append(morseMap.get(temp.charAt(j)+""));
+			}
+			
+			dictionaryMap.put(sb.toString(), temp);
+		}
+		
+		String input = "";
+		while(!(input = br.readLine()).equals("0")) {
+			boolean flag = true;
+			String target = "";
+			int testCase = Integer.parseInt(input);
+			sb = new StringBuilder();
+			
+			for(int i = 0; i < testCase; i++) {
+				String inputMorse = br.readLine();
+				
+				if(dictionaryMap.containsKey(inputMorse)) {
+					sb.append(dictionaryMap.get(inputMorse) + " ");
+				} else {
+					if(flag) {
+						target = inputMorse;
+					}
+					flag = false;
+				}
+			}
+			
+			if(flag) {
+				System.out.println(sb.deleteCharAt(sb.length() - 1));
+			} else {
+				System.out.println(target + " not in dictionary.");
+			}
+		}
+		
 	}
 	
-	// 파일 정리
+	// 회전하는 큐
 	public static void test04() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] idxs = new int[m];
+		st = new StringTokenizer(br.readLine());
+
+		for(int i = 0; i < m; i++) {
+			idxs[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		LinkedList<Integer> dq = new LinkedList<>();
+		
+		for(int i=1; i<n+1; i++) dq.offer(i);
+		
+		int count = 0;
+		for(int idx : idxs) {
+			
+			while(true) {
+				
+				if(dq.peek() == idx) {
+					
+					dq.poll();
+					break;
+					
+				} else {
+					
+					if(dq.indexOf(idx) < (double)dq.size() / 2) {
+						
+						while(dq.peek() != idx) {
+							dq.offerLast(dq.pollFirst());
+							count++;
+						}
+						
+					} else {
+						
+						while(dq.peek() != idx) {
+							dq.offerFirst(dq.pollLast());
+							count++;
+						}
+						
+					}
+				}
+			}
+		}
+		
+		System.out.println(count);
 		
 		
 	}
 
-	// 꿍의 여친 만들기 
+	// 카드
 	public static void test05() throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        Map<Long, Integer> map = new TreeMap<>();
         
+        for(int i = 0; i < n; i++) {
+        	Long num = Long.parseLong(br.readLine());
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+
+        int max = 0;
+        Long target = (long) 0;
+        for (Long key : map.keySet()) {
+			
+        	if(map.get(key) > max) {
+        		max = map.get(key);
+        		target = key;
+        	} else if(map.get(key) == max) {
+        		if(target > key) {
+        			target = key;
+        		}
+        	}
+		}
+        
+		System.out.println(target);
 	}
 
-	// 생태학
+	// 
 	public static void test06() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
