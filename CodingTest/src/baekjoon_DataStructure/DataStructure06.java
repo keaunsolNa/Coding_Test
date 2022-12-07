06c36cf1cf3a7632ca32fb292c7094ab9948ee77
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2022-12-07 19:12:24 +0900

    2022.12.07 자료구조 코딩테스트

--- CodingTest/src/baekjoon_DataStructure/DataStructure06.java
+++ CodingTest/src/baekjoon_DataStructure/DataStructure06.java
@@ -3,17 +3,24 @@ package baekjoon_DataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class DataStructure06 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		test05();
+		test10();
 	}
 	
 	// 절댓값 힙
@@ -244,33 +251,244 @@ public class DataStructure06 {
 		System.out.println(target);
 	}
 
-	// 
+	// 행운의 수
 	public static void test06() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+    	
+		for(int i = 0; i < T; i++) {
+			int length = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			int[] AArr = new int[length];
+			for(int a = 0; a < length; a++) {
+				AArr[a] = Integer.parseInt(st.nextToken());
+				
+			}
+			
+			length = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			int[] BArr = new int[length];
+			for(int a = 0; a < length; a++) {
+				BArr[a] = Integer.parseInt(st.nextToken());
+				
+			}
+			
+			length = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			int[] CArr = new int[length];
+			for(int a = 0; a < length; a++) {
+				CArr[a] = Integer.parseInt(st.nextToken());
+				
+			}
+			
+	    	Set<String> answer = new HashSet<>();
+	    	
+	    	for (int str1 : AArr) {
+				
+	    		for (int str2 : BArr) {
+	    			
+	    			for (int str3 : CArr) {
+	    				String target = (str1 + str2 + str3) + "";
+	    				
+	    				boolean flag = true;
+	    				
+	    				for(int chk = 0; chk < target.length(); chk++) {
+	    					if(target.charAt(chk) != '5' && target.charAt(chk) != '8') {
+	    						flag = false;
+	    					}
+	    				}
+	    				
+	    				if(flag) {
+	    					answer.add(target);
+	    				}
+	    			}
+	    		}
+			}
+			System.out.println(answer.size());
+			
+		}
 	}
 
-	// 최대 힙
+	// 걸그룹 마스터 준석이
 	public static void test07() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, List<String>> girlGroup = new TreeMap<>();
+		
+		for(int i = 0; i < N; i++) {
+			List<String> memberName = new ArrayList<>();
+			String teamName = br.readLine();
+			int groupPeople = Integer.parseInt(br.readLine());
+			
+			for(int j = 0; j < groupPeople; j++) {
+				String member = br.readLine();
+				memberName.add(member);
+			}
+			
+			Collections.sort(memberName);
+			girlGroup.put(teamName, memberName);
+		}
+		
+		for(int i = 0; i < M; i++) {
+			String name = br.readLine();
+			int quiz = Integer.parseInt(br.readLine());
+			
+			if(quiz == 0) {
+				for (String member : girlGroup.get(name)) {
+					System.out.println(member);
+				}
+			} else {
+				for(String team : girlGroup.keySet()) {
+					if(girlGroup.get(team).contains(name)) {
+						System.out.println(team);
+					}
+				}
+			}
+		}
+		
 	}
 
-	// 최소 힙 
+	// I AM IRONMAN 
 	public static void test08() throws NumberFormatException, IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        int W = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        
+        Map<String, String> player = new HashMap<>();
+        for(int i = 0; i < P; i++) {
+        	String[] temp = br.readLine().split(" ");
+        	player.put(temp[0], temp[1]);
+        }
+        
+        int totalGrade = 0;
+        boolean ironMan = true;
+        
+        for(int i = 0; i < N; i++) {
+        	String name = br.readLine();
+        	if(!player.containsKey(name)) {
+        		totalGrade -= L;
+        		if(totalGrade < 0) {
+        			totalGrade = 0;
+        		}
+        	} else if(player.get(name).equals("W")) {
+        		totalGrade += W;
+        	} else {
+        		totalGrade -= L;
+        		if(totalGrade < 0) {
+        			totalGrade = 0;
+        		}
+        	}
+        	
+        	if(totalGrade >= G) {
+        		ironMan = false;
+        	}
+        	
+        }
+        
+        if(ironMan) {
+        	System.out.println("I AM IRONMAN!!");
+        } else {
+        	System.out.println("I AM NOT IRONMAN!!");
+        }
 	}
 
-	// N번째 큰 수
+	// 풍선 터뜨리기
 	public static void test09() throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
+        Deque<int[]> ballon = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("1 ");
+		int target = arr[0];
+		
+		for(int i = 1; i < N; i++){
+			ballon.add(new int[] {(i+1), arr[i]}); 
+		}
+		
+		while(!ballon.isEmpty()) {
+
+			if(target > 0) {
+				for(int i = 1; i < target; i++) {
+					ballon.add(ballon.poll());
+				}
+				
+				int[] nxt = ballon.poll();
+				target = nxt[1];
+				sb.append(nxt[0]+ " ");
+			}
+			
+			else {
+				for(int i = 1; i < -target; i++	) {
+					ballon.addFirst(ballon.pollLast());
+				}
+				
+				int[] nxt = ballon.pollLast();
+				target = nxt[1];
+				sb.append(nxt[0]+" ");
+			}
+		}
+				
+		System.out.println(sb.toString());
 	}
 
-	// 세 번째
+	// Letter Bank
 	public static void test10() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			String bank = st.nextToken();
+			String str = st.nextToken();
+			boolean flag = true;
+			for(int j = 0; j < str.length(); j++) {
+				if(countChar(bank, str.charAt(j)) <= 0) {
+					flag = false;
+					break;
+				}
+			}
+			
+			for(int j = 0 ; j < bank.length(); j++) {
+				if(countChar(str, bank.charAt(j)) <= 0) {
+					flag = false;
+					break;
+				}
+			}
+			
+			if(flag) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
 	}
+	
+    public static long countChar(String str, char ch) {
+        return str.chars()
+                .filter(c -> c == ch)
+                .count();
+    }
 
 
 
