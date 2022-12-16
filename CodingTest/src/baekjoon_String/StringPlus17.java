1b153be62381fdc3fb4108733de93e5acb1b1640
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2022-12-16 17:59:56 +0900

    2022.12.16 문자열 코딩 테스트

--- CodingTest/src/baekjoon_String/StringPlus17.java
+++ CodingTest/src/baekjoon_String/StringPlus17.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class StringPlus17 {
 	
 	public static void main(String[] args) throws IOException {
-		test03();
+		test07();
 	}
 
 	// 문서 검색
@@ -59,34 +59,197 @@ public class StringPlus17 {
         }
 	}
 
-	// 
+	// 진수 변환기
 	public static void test03() throws IOException {
+		BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		if(m == 0) {
+			System.out.println(0);
+		} else {
+			conversion(m, n);
+		}
 	}
 	
-	// 
+    public static void conversion(int number, int N){
+        StringBuilder sb = new StringBuilder();
+	    int current = number;
+	    
+        while(current > 0) {
+            if(current % N < 10){
+
+            	sb.append(current % N);
+                
+            } else {
+            	
+                sb.append((char)(current % N - 10 + 'A'));
+                
+            }
+            current /= N;
+        }
+        System.out.println(sb.reverse());
+    }
+    
+	// 시간복잡도를 배운 도도
 	public static void test04() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
-		int n = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		String pattern = "[a-z]";
+		
+		int max = 0;
+		for(int i = 0; i < C; i++) {
+			String input = br.readLine();
+			input = input.replaceAll("for", "0");
+			input = input.replaceAll("while", "0");
+			input = input.replaceAll(pattern, "");
+			
+			if(max < input.length()) {
+				max = input.length();
+			}
+		}
+		
+		System.out.print(max);
 	}
 	
-	// 
+	// 단어 시계
 	public static void test05() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int h = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
+		String[] min = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", 
+						"thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", 
+						"twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", 
+						"twenty nine", "half" };
+        
+		String[] hour = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen"}; 
+		
+		
+		if(m == 0) {
+			
+			sb.append(hour[h]);
+			sb.append(" o' clock");
+			
+		} else if(m <= 30) {
+			sb.append(min[m]);
+			
+			if(m != 1) {
+				if(m != 15 && m != 30 && m!= 45)
+				sb.append(" minutes");
+			} else {
+				if(m != 15 && m != 30 && m!= 45)
+				sb.append(" minute");
+			}
+			sb.append(" past ");
+			sb.append(hour[h]);
+			
+		} else {
+			
+			m = 60 - m;
+			sb.append(min[m]);
+			
+			if(m != 1) {
+				if(m != 15 && m!= 30 && m != 45) {
+					sb.append(" minute to ");
+				} else {
+					sb.append( " to " );
+				}
+			} else {
+				if(m != 15 && m != 30 && m!= 45) {
+					sb.append(" minutes to ");
+				} else {
+					sb.append(" to ");
+				}
+			}
+			
+			if(h != 12) {
+				sb.append(hour[h + 1]);
+			} else {
+				sb.append(hour[1]);
+			}
+			
+		}
+		
+		System.out.println(sb);
 	}
 	
-    // 
+    // 비트가 넘쳐흘러
 	public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String K = br.readLine();
+		
+		int answer = 0;
+		for(int i = 0; i < K.length(); i++) {
+			if(K.charAt(i) == '1') {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+		
     }
 
-	//
+	// Java vs C++
 	public static void test07() throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        String input = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        
+        if(input.contains("_")) {
+        	
+        	for(int i = 0; i < input.length(); i++) {
+        		boolean flag = false;
+        		if((int)input.charAt(i) < 95) {
+        			sb.setLength(0);
+        			sb.append("Error!");
+        			break;
+        		} else if((int)input.charAt(i) == '_') {
+        			i++;
+        			flag = true;
+        		} else {
+        			sb.append(input.charAt(i));
+        		}
+        		
+        		if(flag) {
+        			sb.append((char)((int)input.charAt(i) - 32));
+        		}
+        	}
+        	
+        } else {
+        	
+        	for(int i = 0; i < input.length(); i++) {
+        		boolean flag = false;
+        		
+        		if(input.charAt(i) < 95) {
+        			sb.append("_");
+        			sb.append((char)((int)input.charAt(i) + 32));
+        		} else {
+        			sb.append(input.charAt(i));
+        		}
+        		
+        	}
+        }
+        
+        System.out.println(sb);
 	}
+	
+    public static long countArrChar(String str, String[] ch) {
+    	long temp = 0;
+    	
+    	for(int i = 0; i < ch.length; i++) {
+    		char cr = ch[i].charAt(0);
+    		temp += str.chars()
+    				.filter(c -> c == cr)
+    				.count();
+    	}
+    	
+    	return temp;
+    }
 	
 	// 
 	public static void test08() throws NumberFormatException, IOException {
