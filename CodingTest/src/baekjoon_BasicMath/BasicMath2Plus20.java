942c3c2c4c9dd715604a65336f7b17faed83117a
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2022-12-19 20:51:47 +0900

    2022.12.19 수학 코딩 테스트

--- CodingTest/src/baekjoon_BasicMath/BasicMath2Plus20.java
+++ CodingTest/src/baekjoon_BasicMath/BasicMath2Plus20.java
@@ -3,12 +3,13 @@ package baekjoon_BasicMath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class BasicMath2Plus20 {
 
    public static void main(String[] args) throws IOException {
-      test05();
+      test10();
    }
    
    // 평균 중앙값 문제
@@ -91,34 +92,102 @@ public class BasicMath2Plus20 {
 	   
    }
    
-   // 
+   // 젓가락 게임
    public static void test06() throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       StringBuilder sb = new StringBuilder();
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      
+      int yt = Integer.parseInt(st.nextToken());
+      int yj = Integer.parseInt(st.nextToken());
+      
+      while(true) {
+    	  yj += yt;
+    	  if(yj >= 5) {
+    		  System.out.println("yt");
+    		  break;
+    	  }
+    	  yt += yj;
+    	  if(yt >= 5) {
+    		  System.out.println("yj");
+    		  break;
+    	  }
+      }
    }
    
-   // 
+   // Not A + B
    public static void test07() throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+       int T = Integer.parseInt(br.readLine());
+       
+       while(T --> 0) {
+    	   StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    	   
+    	   int a = Integer.parseInt(st.nextToken());
+    	   int b = Integer.parseInt(st.nextToken());
+    	   
+    	   if(a + b != 1) {
+    		   System.out.println(1);
+    	   } else {
+    		   System.out.println(2);
+    	   }
+       }
    }
    
-   //
+   // 3의 배수
    public static void test08() throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   StringTokenizer st;
+	   int N = Integer.parseInt(br.readLine());
+	   
+	   int cnt = 0;
+	   for(int i = 3; i < N; i += 3) {
+		   
+		   for(int j = 3; j < N; j+= 3) {
+			   if(i + j >= N) break;
+			   for(int k = 3; j < N; k+= 3) {
+				   if(i + j + k == N) cnt++;
+				   if(i + j + k >= N) break;
+			   }
+		   }
+	   }
+	   
+	   System.out.println(cnt);
    }
    
-   //
+   // e 계산
    public static void test09() throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringBuilder sb = new StringBuilder();
+
+		System.out.printf("");
+		System.out.printf("n e\n");
+		System.out.printf("- -----------\n");
+		System.out.printf("0 1\n");
+		System.out.printf("1 2\n");
+		System.out.printf("2 2.5\n");
+	
+		double factorial = 2;
+		double i = 2;
+		double result = 2.5;
+	
+		while (factorial < 9) {
+			factorial++;
+			i *= factorial;
+			result += 1 / i;
+			System.out.printf("%.0f %.9f\n", factorial, result);
+		}
    }
    
-   // 
+   // 2의 제곱수 계산하기
    public static void test10() throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 	   
+	   float bigNumber = Float.parseFloat(br.readLine());
+	   int cnt = 0;
+	   while(bigNumber % 2 == 0) {
+		   
+		   bigNumber = (bigNumber)/2;
+		   cnt++;
+	   }
+	   
+	   System.out.println(cnt);
    }
 }
