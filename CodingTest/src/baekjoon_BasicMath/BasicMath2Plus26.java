378d3743185a01f70ac61ee2084aa3b9f8eac331
keaunsolNa <knsol1992@naver.com>
2022-12-30 20:31:41 +0900

    2022.12.30 브루트 포스

--- CodingTest/src/baekjoon_BasicMath/BasicMath2Plus26.java
+++ CodingTest/src/baekjoon_BasicMath/BasicMath2Plus26.java
@@ -3,15 +3,14 @@ package baekjoon_BasicMath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class BasicMath2Plus26 {
 
    public static void main(String[] args) throws IOException {
-      test01();
+      test02();
    }
    
    // 보물
@@ -42,11 +41,9 @@ public class BasicMath2Plus26 {
    // 
    public static void test02() throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st;
-      
-	   
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
    }
-   
 
    //
    public static void test03() throws IOException {
--- CodingTest/src/baekjoon_BruteForce/BruteForcePlus03.java
+++ CodingTest/src/baekjoon_BruteForce/BruteForcePlus03.java
@@ -3,14 +3,52 @@ package baekjoon_BruteForce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class BruteForcePlus03 {
 	
-	// 
+	// 회의실 배정
 	public static void test01() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
+		int[][] time = new int[N][2];
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			time[i][0] = Integer.parseInt(st.nextToken());	
+			time[i][1] = Integer.parseInt(st.nextToken());	
+		}
+		
+		
+		Arrays.sort(time, new Comparator<int[]>() {
+			
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				
+				if(o1[1] == o2[1]) return o1[0] - o2[0];
+				
+				return o1[1] - o2[1];
+			}
+ 
+		});
+		
+		int count = 0;
+		int prev_end_time = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			if(prev_end_time <= time[i][0]) {
+				prev_end_time = time[i][1];
+				count++;
+			}
+		}
+		
+		System.out.println(count);
 	}
 
 	// 
