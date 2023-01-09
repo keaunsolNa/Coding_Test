de717b5f9506b214e3ddcd02195ea970c84107e7
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2023-01-09 13:39:27 +0900

    2023.07.09 그리디

--- CodingTest/src/baekjoon_Greedy/Greedy04.java
+++ CodingTest/src/baekjoon_Greedy/Greedy04.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Greedy04 {
 	public static void main(String[] args) throws IOException {
-		test07();
+		test10();
 	}
 	
 	// 팔찌 만들기
@@ -210,27 +211,102 @@ public class Greedy04 {
 		
 	}
 
-	// 
+	// 욱제는 도박쟁이야!!
 	public static void test08() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] first = new int[N];
+        int[] second = new int[N];
         
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) first[i] = Integer.parseInt(st.nextToken());
         
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) second[i] = Integer.parseInt(st.nextToken());
+        
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+        	sum += Math.abs(first[i]);
+        	sum += Math.abs(second[i]);
+        }
+        System.out.println(sum);
 	}
 
-	//
+	// 부분 문자열
 	public static void test09() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String N = br.readLine();
-		int ans = 0;
+		StringTokenizer st;
+		String input = "";
 		
+		while((input = br.readLine()) != null) {
+			st = new StringTokenizer(input);
+			
+			String s = st.nextToken();
+			String t = st.nextToken();
+			int idx = 0;
+			
+			for (int i = 0; i < t.length(); i++) {
+				if (s.charAt(idx) == t.charAt(i)) idx++;
+				if (idx == s.length()) break;
+			}
+			
+			if(idx == s.length()) System.out.println("Yes");
+			else System.out.println("No");
+		}
 	}
 	
-	//
+	// 카우버거
 	public static void test10() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
+		st = new StringTokenizer(br.readLine());
+		
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		int beforeSale = 0;
+		
+		Integer[] burger = new Integer[B];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < B; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			burger[i] = temp;
+			beforeSale += temp;
+		}
+		
+		Integer[] cide = new Integer[C];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < C; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			cide[i] = temp;
+			beforeSale += temp;
+		}
+		
+		Integer[] drink = new Integer[D];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < D; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			drink[i] = temp;
+			beforeSale += temp;
+		}
+		
+		System.out.println(beforeSale);
+		Arrays.sort(burger, Collections.reverseOrder());
+		Arrays.sort(cide, Collections.reverseOrder());
+		Arrays.sort(drink, Collections.reverseOrder());
+		
+		int min = Math.min(Math.min(burger.length, cide.length), drink.length);
+		
+		int afterSale = 0;
+		
+		for(int i = 0; i < min; i++) afterSale += (burger[i] + cide[i] + drink[i]) * 0.9;
+		for(int i = min; i < burger.length; i++) afterSale += burger[i];
+		for(int i = min; i < cide.length; i++) afterSale += cide[i];
+		for(int i = min; i < drink.length; i++) afterSale += drink[i];
+		
+		System.out.println(afterSale);
 	}
 	
 	
