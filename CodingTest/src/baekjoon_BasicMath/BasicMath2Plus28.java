85180d4b9e1e67d0222345752a9a9b8cebe060e2
keaunsolNa <knsol1992@naver.com>
2023-01-11 22:10:28 +0900

    2023.01.11 Math

--- CodingTest/src/baekjoon_BasicMath/BasicMath2Plus28.java
+++ CodingTest/src/baekjoon_BasicMath/BasicMath2Plus28.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class BasicMath2Plus28 {
 
    public static void main(String[] args) throws IOException {
-      test05();
+      test10();
    }
    
    // Ahmed Aly 
@@ -143,38 +143,90 @@ public class BasicMath2Plus28 {
 	   
    }
    
-   // 
+   // Vending Machine
    public static void test06() throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st;
-       String input = "";
+       String[] button = br.readLine().split(" ");
        
-
+       int ans = 5000;
+       for(int i = 0; i < button.length; i++) {
+    	   
+    	   switch(button[i]) {
+    	   
+    	   		case "1" : ans -= 500; break;
+    	   		case "2" : ans -= 800; break;
+    	   		case "3" : ans -= 1000; break;
+    	   	
+    	   }
+       }
+       System.out.println(ans);
    }
    
-   // 
+   // Don’t pass me the ball!
    public static void test07() throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
+       int input = Integer.parseInt(br.readLine());
        
+       System.out.println((input - 3) * (input - 2) * (input - 1) / 6);
    }
    
-   // 
+   // Incomplete Book
    public static void test08() throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   StringTokenizer st = new StringTokenizer(br.readLine());
+	   int A = Integer.parseInt(st.nextToken());
+	   int B = Integer.parseInt(st.nextToken());
+	   
+	   int cnt = 0;
+	   for(int i = A; i <= B; i *= 2) {
+		   B -= i;
+		   cnt++;
+		   if(B < 0) break;
+	   }
+	   
+	   System.out.println(cnt);
    }
    
-   // 
+   // ABCD-код
    public static void test09() throws IOException {
-	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   StringTokenizer st = new StringTokenizer(br.readLine());
-   }
-   
-   // 
-   public static void test10() throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   int T = Integer.parseInt(br.readLine());
 	   
+	   while(T --> 0) {
+		   String input = br.readLine();
+		   int A = Integer.parseInt(input.substring(0, 2)) * Integer.parseInt(input.substring(0, 2));
+		   int B = Integer.parseInt(input.substring(2, 4)) * Integer.parseInt(input.substring(2, 4));
+		   
+		   if((A + B) % 7 == 1) System.out.println("YES");
+		   else System.out.println("NO");
+	   }
+   }
+   
+   // Ratownik 
+   public static void test10() throws IOException {
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   StringTokenizer st;
+	   
+	   st = new StringTokenizer(br.readLine());
+	   int n = Integer.parseInt(st.nextToken());
+	   int k = Integer.parseInt(st.nextToken());
+	   int x = Integer.parseInt(st.nextToken());
+	   int y = Integer.parseInt(st.nextToken());
+	   
+	   int cnt = 0;
+	   for(int i = 0; i < n; i++) {
+
+		   st = new StringTokenizer(br.readLine());
+		   
+		   int x1 = Integer.parseInt(st.nextToken());
+		   int y1 = Integer.parseInt(st.nextToken());
+		   
+		   double dis = (Math.abs(x1 - x) * Math.abs(x1 - x)) + (Math.abs(y1 - y) * Math.abs(y1 - y));
+		   dis = Math.sqrt(dis);
+		   
+		   if(dis > k) cnt++;
+	   }
+	   
+	   System.out.println(cnt);
    }
 }
