5e93255784902748ec20f183063027cad95197e4
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2023-01-18 17:40:55 +0900

    2023.01.18 String

--- CodingTest/src/baekjoon_String/StringPlus28.java
+++ CodingTest/src/baekjoon_String/StringPlus28.java
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
 public class StringPlus28 {
 	
 	public static void main(String[] args) throws IOException {
-		test05();
+		test10();
 	}
 
 	// Vikingahackare
@@ -174,39 +174,138 @@ public class StringPlus28 {
         return result;
     }
 
-    // 
+    // Liczenie punktów 
 	public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 		String input = br.readLine();
+		
+		int cnt = 0;
+		
+		for(int i = 0; i < T; i += (T / 10)) {
+			boolean chk = true;
+			for(int j = i; j < i + (T / 10); j++) {
+				if(input.charAt(j) != 'T') chk = false;
+			}
+			
+			if(chk) cnt++;
+		}
+		
+		System.out.println(cnt);
 	}
 	
-	// 
+	// Poker Hand
 	public static void test07() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String input = br.readLine();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			Map<Character, Integer> map = new HashMap<>();
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < 5; i++) {
+				char key = st.nextToken().charAt(0);
+				map.put(key, map.getOrDefault(key, 0) + 1);
+			}
+
+			int max = 0;
+			for(char key : map.keySet()) if(max < map.get(key)) max = map.get(key);
+			
+			System.out.println(max);
+		}
 	}
 	
-	// 
+	// Square 
 	public static void test08() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String input = br.readLine();
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			String input = br.readLine();
+			int length = input.length();
+			
+			for(int i = 0; i < length; i++) {
+				sb.append(input.charAt(i));
+				if(i == 0) 
+					for(int j = i + 1; j < length; j++) sb.append(input.charAt(j));
+				
+				
+				if(i != 0 && i != length -1) {
+					for(int j = 1; j < length - 1; j++) sb.append(" ");
+					sb.append(input.charAt(length - i - 1));
+				}
+				
+				if(i == length -1) 
+					for(int j = length - 2; j >= 0; j--) sb.append(input.charAt(j));
+				
+				sb.append("\n");
+			}
+			
+			
+		}
+		
+		System.out.println(sb);
 	}
 	
-	// 
+	// Thai Lottery Checking
 	public static void test09() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String input = br.readLine();
+		StringTokenizer st;
+		
+		Map<String, Integer> map = new HashMap<>();
+		String[] number = new String[6];
+		long[] money = new long[6];
+		for(int i = 0; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			number[i] = st.nextToken();
+			money[i] = Long.parseLong(st.nextToken());
+			
+		}
+		
+		String input = "0";
+		
+		while(!(input = br.readLine()).equals("-1")) {
+			
+			long price = 0;
+			
+			String front = input.substring(0, 3);
+			String back = input.substring(3, 6);
+			String back2 = input.substring(4, 6);
+			
+			if(number[0].equals(input)) price += money[0];
+			if(number[1].equals(front)) price += money[1];
+			if(number[2].equals(front)) price += money[2];
+			if(number[3].equals(back)) price += money[3];
+			if(number[4].equals(back)) price += money[4];
+			if(number[5].equals(back2)) price += money[5];
+			
+			sb.append(price + "\n");
+		}
+		
+		System.out.print(sb);
 	}
 	
-	// 
+	// Cryptographer’s Conundrum
 	public static void test10() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		String input = br.readLine();
+		
+		int cnt = 0;
+		for(int i = 0; i < input.length(); i += 3) {
+			
+			if(input.charAt(i) != 'P') cnt++;
+			if(input.charAt(i + 1) != 'E') cnt++;
+			if(input.charAt(i + 2) != 'R') cnt++;
+		}
+		
+		System.out.println(cnt);
 	}
 	
 	
