2226bbb9a120dabc175f928a16c1f889dcaca201
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2023-02-06 19:33:46 +0900

    2023.02.06 Graph

---
+++ CodingTest/src/baekjoon_GraphTheory/GraphTheory01.java
@@ -0,0 +1,102 @@
+package baekjoon_GraphTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class GraphTheory01 {
+	public static void main(String[] args) throws IOException {
+		test02();
+	}
+	
+	// 9372번 - 상근이의 여행
+	public static void test01() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			for(int i = 0; i < M; i++) br.readLine();
+			
+			sb.append((N - 1) + "\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+	// 
+	public static void test02() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+	}
+	
+	
+	// 
+	public static void test03() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test04() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test05() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test06() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test07() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test08() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test09() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+	// 
+	public static void test10() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+	
+}
