e5c22019d42ce95410275f98f5b9f032d25a7efb
keaunsolNa <knsol1992@naver.com>
2023-02-10 00:00:42 +0900

    23.02.10 Geometry

--- CodingTest/src/baekjoon_Geometry/Geometry01.java
+++ CodingTest/src/baekjoon_Geometry/Geometry01.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 public class Geometry01 {
 	public static void main(String[] args) throws IOException {
-		test05();
+		test09();
 	}
 	
 	// 1297번 - TV 크기
@@ -110,7 +110,7 @@ public class Geometry01 {
 		else System.out.println((int) d);
 	}
 	
-	// 5617qjs - 問題 1
+	// 5617번 - 問題 1
 	public static void test05() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -150,39 +150,134 @@ public class Geometry01 {
 		
 	}
 	
-	//
+	// 15449번 -Art
 	public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[5];
+		for(int i = 0; i < 5; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		int cnt = 0;
+		for(int i = 0; i < 5; i++) {
+			
+			for(int j = i + 1; j < 5; j++) {
+				
+				for(int k = j + 1; k < 5; k++) {
+					
+					if(arr[i] + arr[j] > arr[k]) {
+						
+						if((arr[i] + arr[j] + arr[k]) / 2 > 0 )
+						cnt++;
+					}
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+		
 	}
 	
-	//
+	// 8716번 - Pole 
 	public static void test07() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		int x4 = Integer.parseInt(st.nextToken());
+		int y4 = Integer.parseInt(st.nextToken());
+		
+		if(x2 < x3 || x1 > x4 || y1 < y4 || y2 > y3) System.out.println(0);
+	    
+	    else {
+	    	long lx = Math.max(x1, x3);
+	    	long rx = Math.min(x2, x4);
+	    	long ry = Math.max(y2, y4);
+	    	long ly = Math.min(y1, y3);
+	    	
+	    	long width = rx - lx;
+	    	long height = ry - ly;
+	    	
+	    	System.out.println(Math.abs(width * height));
+	    }
+		
 	}
 	
-	//
+	// 2756번 - 다트 
 	public static void test08() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= T; i++) {
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			double[] dart = new double[12];
+			for(int j = 0; j < 12; j++) dart[j] = Math.abs(Double.parseDouble(st.nextToken()));
+			
+			double P1 = 0;
+			double P2 = 0;
+			int tern = 1;
+			
+			for(int j = 0; j < 12;) {
+				
+				for(int k = 0; k < 3; k++) {
+					double dis = dart[j] * dart[j] + dart[j + 1] * dart[j + 1];
+					if(tern == 1) 
+						P1 += dis <= 9 ? 100 : dis <= 36 ? 80 : dis <= 81 ? 60 : dis <= 144 ? 40 : dis <= 225 ? 20 : 0;
+					else 
+						P2 += dis <= 9 ? 100 : dis <= 36 ? 80 : dis <= 81 ? 60 : dis <= 144 ? 40 : dis <= 225 ? 20 : 0;
+					j += 2;
+				}
+				tern = tern == 1 ? 2 : 1;
+				
+			}
+			
+			String P1S = String.format("%.0f", P1);
+			String P2S = String.format("%.0f", P2);
+			sb.append("SCORE: " + P1S + " to " + P2S + ", ");
+			if(P1 > P2) sb.append("PLAYER 1 WINS.");
+			else if(P2 > P1) sb.append("PLAYER 2 WINS.");
+			else sb.append("TIE.");
+			
+			sb.append("\n");
+		}
+
+		System.out.print(sb);
 	}
 	
-	//
+	// 16480번 - 외심과 내심은 사랑입니다
 	public static void test09() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long R = Long.parseLong(st.nextToken());
+		long r = Long.parseLong(st.nextToken());
+		
+		System.out.print((R * (R -2 * r)));
 	}
 	
-	//
+	// 16481번 - 원 전문가 진우 
 	public static void test10() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long r1 = Long.parseLong(st.nextToken());
+		long r2 = Long.parseLong(st.nextToken());
+		long r3 = Long.parseLong(st.nextToken());
+		
+		System.out.printf("%.10f", (double)(r1 * r2 * r3) / (r1 * r2 + r2 * r3 + r1 * r3));
 	}
 	
 	
