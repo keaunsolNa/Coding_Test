a57917ef74ea5a48302a6a52e96d64401374e3c4
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2023-02-20 19:24:32 +0900

    2023.02.20 Geometry

--- CodingTest/src/baekjoon_Geometry/Geometry02.java
+++ CodingTest/src/baekjoon_Geometry/Geometry02.java
@@ -26,7 +26,7 @@ public class Geometry02 {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		test01();
+		test05();
 	}
 	
 	// 13411번 - 하늘에서 정의가 빗발친다!
@@ -64,37 +64,149 @@ public class Geometry02 {
 		System.out.println(sb);
 	}
 	
-	// 
+	// 11880번 - 개미
 	public static void test02() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
+		int T = Integer.parseInt(br.readLine());
 		
+		while(T --> 0) {
+			
+			st = new StringTokenizer(br.readLine());
+			long a = Integer.parseInt(st.nextToken());
+			long b = Integer.parseInt(st.nextToken());
+			long c = Integer.parseInt(st.nextToken());
+			
+			long z = (a * a) + (b * b) + (c * c);
+			long min = Math.min(a * b, b * c);
+			min = Math.min(min, a * c);
+			
+			sb.append(z + 2 * min + "\n");
+		}
+		
+		System.out.println(sb);
+			
 	}
 	
-	// 
+	// 10540번 - KLOPKA
 	public static void test03() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		int T = Integer.parseInt(br.readLine());
 		
+		int maxX = Integer.MIN_VALUE;
+		int minX = Integer.MAX_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int minY = Integer.MAX_VALUE;
+		
+		while(T --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			maxX = Math.max(x, maxX);
+			minX = Math.min(x, minX);
+			maxY = Math.max(y, maxY);
+			minY = Math.min(y, minY);
+		}
+		
+		int max = Math.max(maxX - minX, maxY - minY);
+		System.out.println(max * max);
 	}
 	
-	// 
+	// 6013번 - Lonesome Partners
 	public static void test04() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
 		
+		int[][] map = new int[T][2];
+		for(int i = 0; i < T; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			map[i][0] = x;
+			map[i][1] = y;
+		}
+		
+		double max = Double.MIN_VALUE;
+		int[] ans = new int[2];
+		for(int i = 0; i < T; i++) {
+			
+			for(int j = i + 1; j < T; j++) {
+				
+				double dis = Math.pow(map[i][0] - map[j][0], 2) + Math.pow(map[i][1] - map[j][1], 2);
+				
+				if(max < dis) {
+					max = dis;
+					ans[0] = i + 1;
+					ans[1] = j + 1;
+				}
+			}
+		}
+		
+		System.out.println(ans[0] + " " + ans[1]);
 		
 	}
 	
-	// 
+	// 13552번 - 구와 쿼리
 	public static void test05() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
+		int N = Integer.parseInt(br.readLine());
 		
+		int[][] dot = new int[N][3];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int z = Integer.parseInt(st.nextToken());
+			
+			dot[i][0] = x;
+			dot[i][1] = y;
+			dot[i][2] = z;
+			
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < M; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int z = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			
+			long rPow = 1l*r*r;
+			int chk = 0;
+			
+			for(int j = 0; j < dot.length; j++) {
+				
+				long dis = 1l * (dot[j][0] - x) * (dot[j][0] - x) 
+						 + 1l * (dot[j][1] - y) * (dot[j][1] - y) 
+						 + 1l * (dot[j][2] - z) * (dot[j][2] - z);
+				
+				if(dis <= rPow) chk++;
+			}
+			
+		
+			sb.append(chk + "\n");
+		}
+		
+		System.out.println(sb);
 	}
+	
 	
 	// 
 	public static void test06() throws IOException {
