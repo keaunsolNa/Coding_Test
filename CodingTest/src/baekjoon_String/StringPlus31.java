83123e0d681559a10a5782005979a7c3404a2a2d
DESKTOP-4DEJGCM\12192127 <knsol1992@naver.com>
2023-02-22 14:16:55 +0900

    2023.02.22 String

--- CodingTest/src/baekjoon_String/StringPlus31.java
+++ CodingTest/src/baekjoon_String/StringPlus31.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class StringPlus31 {
 	
 	public static void main(String[] args) throws IOException {
-		test04();
+		test10();
 	}
 
 	// 22351번 - 수학은 체육과목 입니다 3
@@ -178,65 +183,205 @@ public class StringPlus31 {
 		System.out.printf("%.6f", sum / totalPoint);
 	}
 	
-	
-	//
+	// 9253번 - 스페셜 저지
 	public static void test05() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
+		String A = br.readLine();
+		String B = br.readLine();
+		String C = br.readLine();
 		
+		if(A.contains(C) && B.contains(C)) System.out.println("YES");
+		else System.out.println("NO");
 	}
 	
-	
-	//
+	// 17176번 - 암호해독기
 	public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
+
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		char[] cryptogram = new char[N];
+		for(int i = 0; i < N; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			
+			if(temp > 26) cryptogram[i] = ((char)(temp + 70));
+			else if(temp > 0) cryptogram[i] = ((char)(temp + 64)) ;
+			else cryptogram[i] = ' ';
+		}
 		
+		Arrays.sort(cryptogram);
+		
+		char[] plainText = br.readLine().toCharArray();
+		Arrays.sort(plainText);
+		
+		for(int i = 0; i < N; i++) {
+			if(plainText[i] != cryptogram[i]) {
+				System.out.println("n");
+				return;
+			}
+		}
+		
+		System.out.println("y");
 	}
 	
-	
-	//
+	// 4446번 - ROT13
 	public static void test07() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
+		String input = "";
+		Integer[] lowerVowels = new Integer[] {97, 105, 121, 101, 111, 117};
+		Integer[] lowerConsonant = new Integer[] {98, 107, 120, 122, 110, 104, 100, 99, 119, 103, 112, 118, 106, 113, 116, 115, 114, 108, 109, 102};
+		Integer[] upperVowels = new Integer[] {65, 73, 89, 69, 79, 85};
+		Integer[] upperConsonant = new Integer[] {66, 75, 88, 90, 78, 72, 68, 67, 87, 71, 80, 86, 74, 81, 84, 83, 82, 76, 77, 70};
 		
+		List<Integer> LV = new ArrayList<Integer>(Arrays.asList(lowerVowels)); 
+		List<Integer> LC = new ArrayList<Integer>(Arrays.asList(lowerConsonant)); 
+		List<Integer> UV = new ArrayList<Integer>(Arrays.asList(upperVowels)); 
+		List<Integer> UC = new ArrayList<Integer>(Arrays.asList(upperConsonant)); 
+		
+		while((input = br.readLine()) != null) {
+			
+			for(int i = 0; i < input.length(); i++) {
+				
+				int temp = (int)input.charAt(i);
+				if(temp > 96 && temp < 123) {
+					
+					if(temp == 97 || temp == 101 || temp == 105 || temp == 111 || temp == 117 || temp == 121) {
+						int index = LV.indexOf(temp);
+						
+						if(index - 3 < 0) sb.append((char)(int)LV.get(index + 3));
+						else sb.append((char)(int)LV.get(index - 3));
+					}
+					
+					else {
+						
+						int index = LC.indexOf(temp);
+						
+						if(index - 10 < 0) sb.append((char)(int)LC.get(index + LC.size() - 10));
+						else sb.append((char)(int)LC.get(index - 10));
+					}
+				}
+				
+				else if(temp > 64 && temp < 91){
+					
+					if(temp == 65 || temp == 73 || temp == 89 || temp == 69 || temp == 79 || temp == 85) {
+						int index = UV.indexOf(temp);
+						
+						if(index - 3 < 0) sb.append((char)(int)UV.get(index + 3));
+						else sb.append((char)(int)UV.get(index - 3));
+					}
+					
+					else {
+						
+						int index = UC.indexOf(temp);
+						
+						if(index - 10 < 0) sb.append((char)(int)UC.get(index + UC.size() - 10));
+						else sb.append((char)(int)UC.get(index - 10));
+					}
+				}
+				
+				else sb.append(input.charAt(i));
+				
+			}
+		
+			sb.append("\n");
+		}		
+		
+		System.out.println(sb);
 	}
 	
-	
-	//
+	// 5426 - 비밀 편지
 	public static void test08() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
+	
+		for(int i = 0; i < T; i++) {
+			String input = br.readLine();
+			int length = input.length();
+			length = (int)Math.sqrt(length);
+			char[][] map = new char[length][length];
+
+			int idx = 0;
+			for(int j = 0; j < length; j++) {
+				
+				for(int k = 0; k < length; k++) {
+					map[j][k] = input.charAt(idx);
+					idx++;
+				}
+			}
+			
+			char[][] ans = new char[length][length];
+			
+			for(int j = 0; j < length; j++) {
+				
+				for(int k = 0; k < length; k++) {
+					
+					ans[j][k] = map[k][length - j - 1];
+				}
+			}
+			
+			for (char[] cs : ans) {
+				for (char cs2 : cs) {
+					sb.append(cs2);
+				}
+			}
+			
+			sb.append("\n");
+		}
 		
+		System.out.println(sb);
 	}
 	
-	
-	//
+	// 2890번 - 카약 
 	public static void test09() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
 		
+		st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < x; i++) {
+			
+			String input = br.readLine();
+			for(int j = 0; j < y; j++) {
+				
+				char temp = input.charAt(j);
+				if(temp != 'S' && temp != '.' && temp != 'F') {
+					map.put(Character.getNumericValue(temp), y - j);
+					break;
+				}
+			}
+		}
+		
+		Set<Integer> set = new HashSet<>();
+		for (int key : map.keySet()) set.add(map.get(key));
+		
+		List<Integer> sortList = new ArrayList<>(set);
+		Collections.sort(sortList);
+		
+		for (Integer key : map.keySet()) System.out.println(sortList.indexOf(map.get(key)) + 1);
 	}
 	
-	
-	//
+	// 25193번 - 곰곰이의 식단 관리
 	public static void test10() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		
+		int N = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+
+		int C = (int)countChar(input, 'C');
+
+		System.out.println(C / (N - C + 1) + (C % (N - C + 1) != 0 ? 1 : 0));
 	}
 	
+	 
+	public static long countChar(String str, char ch) {
+		return str.chars()
+				.filter(c -> c == ch)
+				.count();
+	}
 	
 }
