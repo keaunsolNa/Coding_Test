f0f3afef0fae558035b5ab50fff9114ad84b5bdf
KeaunsolNa <knsol1992@naver.com>
2023-03-20 19:59:37 +0900

    2023.03.20 Implement

--- CodingTest/src/baekjoon_implement/Implement51.java
+++ CodingTest/src/baekjoon_implement/Implement51.java
@@ -4,8 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 
@@ -29,7 +35,7 @@ public class Implement51 {
     private static ArrayList<Node> list;
     
 	public static void main(String[] args) throws IOException {
-		test02();
+		test07();
 	}
 	
 	// 16234번 - 인구 이동
@@ -131,38 +137,230 @@ public class Implement51 {
     }
     
 	// 12919번 A와 B 2
+    private static String S;
+    private static String T;
+    private static int legnth;
 	public static void test02() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringBuilder key = new StringBuilder(br.readLine());
-		StringBuilder T = new StringBuilder(br.readLine());
-		
-		while(T.length() != key.length()) {
-				
-			if(T.charAt(T.length() - 1) == 'A') T.deleteCharAt(T.length() - 1);
-			else {
-				System.out.println(T);
-				T.reverse();
-				T.deleteCharAt(0);
-				System.out.println(T);
-			}
-		}
-		
-		System.out.println(T.equals(key) ? 1 : 0);
+		S = br.readLine();
+		T = br.readLine();
+		legnth = S.length();
+
+		System.out.println(solve(T) ? 1 : 0);
 	}
 	
-	// 
+    private static boolean solve(String str) {
+    	
+    	if(str.length() == legnth) {
+            if(str.equals(S)) return true;
+            return false;
+        }
+
+        if(str.charAt(str.length() -1) == 'A') 
+            if(solve(str.substring(0, str.length() -1))) return true;
+
+        if(str.charAt(0) == 'B') {        
+        	
+            StringBuilder reverse = new StringBuilder();
+            reverse.append(str.substring(1, str.length()));
+            if(solve(reverse.reverse().toString())) return true;
+            
+        }
+        
+        return false;
+    }
+    
+	// 14542번 - Outer Triangle Sum
 	public static void test03() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		String input = "";
+		
+		while(!(input = br.readLine()).equals("0")) {
+			
+			int T = Integer.parseInt(input);
+			int TC = 1;
+			List<List<Long>> arr = new ArrayList<>();
+			
+			for(int i = 0; i < T; i++) {
+			
+				arr.add(new ArrayList<Long>());
+				st = new StringTokenizer(br.readLine());
+				while(st.hasMoreTokens()) 
+					arr.get(i).add(Long.parseLong(st.nextToken()));
+			}
+
+			long sum = 0;
+			for(int i = 0; i < T; i++) sum += arr.get(i).get(0);
+			
+			for(int i = 1; i < T; i++) {
+				
+				if(i == T - 1) for(int j = 1; j < T; j++) sum += arr.get(i).get(j);
+				
+				else if(i > 0 && i < T - 1) {
+					sum += arr.get(i).get(arr.get(i).size() - 1);
+				}
+				
+			}
+			
+			sb.append("Case #" + TC + ":" + sum + "\n");
+			TC++;
+		}
+		
+		System.out.print(sb);
 	}
 	
-	// 
+	// 27736번 - 찬반투표 
 	public static void test04() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
 		st = new StringTokenizer(br.readLine());
+		int aggre = 0;
+		int denial = 0;
+		int abstention = 0;
+		
+		for(int i = 0; i < N; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			
+			if(input == 1) aggre++;
+			else if(input == -1) denial++;
+			else abstention++;
+			
+		}
+		
+		if((double)abstention >= (double)N / 2) System.out.println("INVALID");
+		else if(aggre > denial) System.out.println("APPROVED");
+		else System.out.println("REJECTED");
+		
+	}
+	
+	// 1268번 - 임시 반장 정하기
+	public static void test05() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][5];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			for(int j = 0; j < 5; j++) 
+				arr[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		int cnt = 0;
+		int cnt2 = 0;
+		for(int i = 0; i < N; i++) {
+			
+			Set<Integer> set = new HashSet<>();
+			
+			for(int j = 0; j < 5; j++) {
+				
+				for(int k = 0; k < N; k++) {
+					
+					if(i == k) continue;
+					
+					if(arr[i][j] == arr[k][j]) {
+						set.add(k);
+					}
+				}
+			}
+			
+			if(cnt2 < set.size()) {
+				cnt2 = set.size();
+				cnt = i + 1;
+			}
+		}
+
+		if(cnt == 0) System.out.println(1);
+		else System.out.println(cnt);
+		
+	}
+
+	// 17072번 - 아스키 아트
+	public static void test06() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				
+				int r = Integer.parseInt(st.nextToken());
+				int g = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int rgb = (2126 * r) + (7152 * g) + (722 * b);
+				
+	            if (0 <= rgb && rgb < 510000)
+	            	sb.append(String.format("%c", 35));
+	            else if (510000 <= rgb && rgb < 1020000)
+	            	sb.append(String.format("%c", 111));
+	            else if (1020000 <= rgb && rgb < 1530000)
+	            	sb.append(String.format("%c", 43));
+	            else if (1530000 <= rgb && rgb < 2040000)
+	            	sb.append(String.format("%c", 45));
+	            else
+	            	sb.append(String.format("%c", 46));
+	            
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+	// 20113 - 긴급 회의
+	public static void test07() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) 
+			arr[Integer.parseInt(st.nextToken())]++;
+		
+		int max = 0;
+		int idx = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				idx = i;
+			}
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(idx != i && arr[i] == max) {
+				System.out.println("skipped");
+				return;
+			}
+		}
+		
+		System.out.println(idx);
+	}
+	
+	// 
+	public static void test08() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		
+		
 	}
 	
 	
