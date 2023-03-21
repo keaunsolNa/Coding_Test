e0ef2ab0bb1e197c372bfbbcaa30031ee7acaed0
KeaunsolNa <knsol1992@naver.com>
2023-03-21 19:38:31 +0900

    2023.03.21 Implement

--- CodingTest/src/baekjoon_implement/Implement51.java
+++ CodingTest/src/baekjoon_implement/Implement51.java
@@ -4,12 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -35,7 +32,7 @@ public class Implement51 {
     private static ArrayList<Node> list;
     
 	public static void main(String[] args) throws IOException {
-		test09();
+		test10();
 	}
 	
 	// 16234번 - 인구 이동
@@ -381,23 +378,121 @@ public class Implement51 {
 		System.out.println(ans);
 	}
 	
-	// 
+	// 2980번 - 도로와 신호등
 	public static void test09() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
 		
+		int t1  = 0;
+		int t2  = 0;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int D = Integer.parseInt(st.nextToken());
+			int R = Integer.parseInt(st.nextToken());
+			int G = Integer.parseInt(st.nextToken());
+			
+			t2 += D - t1;
+			t1 = D;
+			
+			int dif = t2 % (R + G);
+			
+			if(dif  < R) t2  += R - dif ;
+		}
+		
+		t2 += L - t1;
+		
+		System.out.println(t2);
 	}
 	
+	
 	// 
+	private static int N; 
+	private static int M;
+	private static int cnt;
+	private static int[][] map2;
+	private static boolean[][] visited2;
+	
+	private static int[] dr = {-1, 0, 1, 0};
+    private static int[] dc = {0, 1, 0, -1};
+    
 	public static void test10() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map2 = new int[N][M];
+		visited2 = new boolean[N][M];
 		
+		st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < N; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < M; j++) 
+				map2[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		pathFind(r, c, d);
+		
+		System.out.println(cnt);
 	}
+    
+	private static void pathFind(int r, int c, int d) {
+		
+    	int nowR = r;
+        int nowC = c;
+        int nowD = d;
+        int stop = 0;
+
+        while(true) {
+        	
+            if(stop == 4) {
+                int backDir = nowD - 2 < 0 ? nowD + 2 : nowD - 2;
+                int backR = nowR + dr[backDir];
+                int backC = nowC + dc[backDir];
+
+                if(map2[backR][backC] == 1) break;
+                
+                else {
+                    nowR = backR;
+                    nowC = backC;
+                    stop = 0;
+                }
+            }
+
+            if(!visited2[nowR][nowC]) {
+                visited2[nowR][nowC] = true;
+                cnt++;
+            }
+
+            int nextDir = nowD - 1 < 0 ? nowD + 3 : nowD - 1;
+            int nextR = nowR + dr[nextDir];
+            int nextC = nowC + dc[nextDir];
+
+            if(!visited2[nextR][nextC] && map2[nextR][nextC] == 0) {
+                nowR = nextR;
+                nowC = nextC;
+                nowD = nextDir;
+                stop = 0;
+            }
+            
+            else {
+            	nowD = nextDir;
+                stop++;
+            }
+        }
 	
-	
+	}
 }
