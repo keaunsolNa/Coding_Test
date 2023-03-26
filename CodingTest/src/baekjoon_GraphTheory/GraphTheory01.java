a6a8b86e916984c1b7f30fc19275ad59623ead1b
KeaunsolNa <knsol1992@naver.com>
2023-03-26 21:38:43 +0900

    2023.03.26 Graph

--- CodingTest/src/baekjoon_GraphTheory/GraphTheory01.java
+++ CodingTest/src/baekjoon_GraphTheory/GraphTheory01.java
@@ -9,13 +9,14 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class GraphTheory01 {
 	public static void main(String[] args) throws IOException {
-		test05();
+		test08();
 	}
 	
 	// 9372번 - 상근이의 여행
@@ -192,25 +193,215 @@ public class GraphTheory01 {
 		System.out.println(res);
 	}
 	
-	// 
+	// 2206번 - 벽 부수고 이동하기
+	private static int[] dx = {1, 0, -1, 0};
+	private static int[] dy = {0, 1, 0, -1};
 	public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+        if(N-1 == 0 && M-1 == 0){
+            System.out.print(1);
+            System.exit(0);
+        }
+
+        char[][] miro = new char[N][M];  
+        int[][] dist = new int[N][M];   
+        boolean[][][] visit = new boolean[2][N][M];    
+        Queue<int[]> qu = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                miro[i][j] = s.charAt(j);
+            }
+        }
+
+        qu.offer(new int[]{0, 0, 0});   
+
+        while (!qu.isEmpty()) {
+            int[] cur = qu.poll();  
+            
+            for(int i = 0; i < 4; i++) {
+                int nX = cur[0] + dx[i];
+                int nY = cur[1] + dy[i];
+
+                if (nX < 0 || nX >= N || nY < 0 || nY >= M) continue;
+                
+                
+                if (miro[nX][nY] == '1') {
+                    if(cur[2] == 0 && !visit[1][nX][nY]){ 
+                        visit[cur[2]][nX][nY] = true;    
+                        dist[nX][nY] = dist[cur[0]][cur[1]] + 1; 
+                        qu.offer(new int[]{nX, nY, 1});   
+                    }
+                }
+                
+                else{
+                	
+                    if(!visit[cur[2]][nX][nY]) {
+                    	
+                        visit[cur[2]][nX][nY] = true;    
+                        dist[nX][nY] = dist[cur[0]][cur[1]] + 1; 
+                        qu.offer(new int[]{nX, nY, cur[2]});
+                        
+                    }
+                }
+                
+                if(nX == N - 1 && nY == M - 1) {
+                    System.out.print(dist[nX][nY] + 1);
+                    System.exit(0);
+                }
+            }
+        }
+        
+        System.out.print(-1);
 	}
 	
-	// 
+	// 16236번 - 아기 상어
 	public static void test07() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		
+        int[] cur = null;
+
+        // 맵 설정 및 초기 상어값 cur 변수 할당
+        for (int i = 0; i < N; i++) {
+        	
+        	st = new StringTokenizer(br.readLine());
+        	
+        	for (int j = 0; j < N; j++) {
+        		map[i][j] = Integer.parseInt(st.nextToken());
+        		
+        		if (map[i][j] == 9) {
+        			cur = new int[]{i, j};
+        			map[i][j] = 0;
+        		}
+        		
+        	}	
+        }
+        
+        // 최초 사이즈
+        int nowSize = 2;
+        
+        // 먹은 물고기 숫자
+        int eat = 0; 
+        
+        // 움직인 거리
+        int move = 0; 
+        
+        while (true) {
+        	
+        	// 우선순위 큐. 
+            PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) ->
+                    o1[2] != o2[2] 
+                    				? Integer.compare(o1[2], o2[2]) : (o1[0] != o2[0] 
+                    				? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]))
+            
+            		);
+            
+            // dfs visit 변수
+            boolean[][] visit = new boolean[N][N];
+
+            // 우선순위 큐에 x, y 좌표. 총 이동 거리 int 배열 초기값
+            que.add(new int[]{cur[0], cur[1], 0});
+            
+            visit[cur[0]][cur[1]] = true;
+
+            boolean ck = false; 
+
+            while (!que.isEmpty()) {
+            	
+                cur = que.poll();
+
+                // map에 먹이가 존재하고 상어의 사이즈보다 작은 경우
+                if (map[cur[0]][cur[1]] != 0 && map[cur[0]][cur[1]] < nowSize) { 
+                	
+                	// 해당 위치 물고기 제거
+                    map[cur[0]][cur[1]] = 0; 
+                    eat++; 
+                    move += cur[2]; 
+                    ck = true; 
+                    break;
+                    
+                }
+
+                for (int k = 0; k < 4; k++) {
+                	
+                    int ny = cur[0] + dy[k];
+                    int nx = cur[1] + dx[k];
+
+                    if (ny < 0 || nx < 0 || nx >= N || ny >= N || visit[ny][nx] || map[ny][nx] > nowSize) continue;
+
+                    que.add(new int[]{ny, nx, cur[2] + 1});
+                    visit[ny][nx] = true;
+                    
+                }
+            }
+
+            // 큐가 비워질 때까지 먹이를 먹은적이 없다면, 더 이상 먹은 물고기가 없으므로 탈출
+            if (!ck) break;
+
+            // 사이즈와 먹이를 먹은 수가 동일하다면 상어의 크기를 증가
+            if (nowSize == eat) { 
+            	nowSize++;
+                eat = 0;
+            }
+        }
+        System.out.println(move);
 	}
 	
-	// 
+	// 1766번 - 문제집 (위상정렬)
 	public static void test08() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+		int [] preCnt = new int[N + 1];
+		
+		for(int i = 0; i <= N; ++i) map.add(new ArrayList<>());
+		
+		for(int i = 0; i < M; ++i) {
+			
+            st = new StringTokenizer(br.readLine());
+            int prev = Integer.parseInt(st.nextToken());
+            int next = Integer.parseInt(st.nextToken());
+
+            map.get(prev).add(next);
+            preCnt[next]++;
+            
+        }
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+	        
+		for(int i = 1; i <= N; ++i) if(preCnt[i] == 0) pq.add(i);
+
+		while(!pq.isEmpty()) {
+			
+            int cur = pq.poll();
+            sb.append(cur).append(" ");
+
+            for(int i : map.get(cur)) {
+            	
+                preCnt[i]--;
+                if(preCnt[i] == 0) pq.add(i);
+                
+            }
+        }
+
+        System.out.print(sb.toString());
+		
 	}
 	
 	// 
