ca39c783ba013d011568c1c687aa1c482df99bda
KeaunsolNa <knsol1992@naver.com>
2023-03-30 20:29:22 +0900

    2023.03.30 BruteForce

--- CodingTest/src/baekjoon_BruteForce/BruteForcePlus06.java
+++ CodingTest/src/baekjoon_BruteForce/BruteForcePlus06.java
@@ -4,14 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BruteForcePlus06 {
 	
    public static void main(String[] args) throws IOException {
-      test03();
+      test06();
    }
 	
    private static class BD {
@@ -26,6 +26,22 @@ public class BruteForcePlus06 {
        }
    }
    
+   private static class Reward implements Comparable<Reward>{
+	   
+	   int money;
+	   int[] need;
+	   
+	   private Reward(int money, int[] need) {
+		   this.money = money;
+		   this.need = need;
+	   }
+	   
+	   @Override
+       public int compareTo(Reward o) {
+       	
+           return o.money - this.money;
+       }
+   }
    
    private static List<BD> data = new ArrayList<>();
    private static int n;
@@ -50,7 +66,7 @@ public class BruteForcePlus06 {
         System.out.println(calBaseBall());
     }
 
-    private static int calBaseBall() {
+   private static int calBaseBall() {
         int result = 0;
 
         for (int i = 123; i <= 987; i++) {
@@ -92,7 +108,7 @@ public class BruteForcePlus06 {
         return result;
     }
 
-    private static boolean checkSameNum(int num) {
+   private static boolean checkSameNum(int num) {
 
         String numString = Integer.toString(num);
 
@@ -107,8 +123,8 @@ public class BruteForcePlus06 {
         return true;
     }
 
-	// 4375번 - 1
-	public static void test02() throws IOException {
+   // 4375번 - 1
+   public static void test02() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
@@ -134,9 +150,9 @@ public class BruteForcePlus06 {
 		System.out.println(sb);
 	}
 	
-	// 2548번 - 대표 자연수
-	public static void test03() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   // 2548번 - 대표 자연수
+   public static void test03() throws IOException {
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         
@@ -177,14 +193,127 @@ public class BruteForcePlus06 {
 		
 	}
 	
-	//
-	public static void test04() throws IOException {
+   // 9329번 - 패스트 푸드 상금
+   public static void test04() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			PriorityQueue<Reward> reward = new PriorityQueue<>();
+			while(n --> 0) {
+				st = new StringTokenizer(br.readLine());
+				
+				int k = Integer.parseInt(st.nextToken());
+				int[] arr = new int[k];
+				
+				for(int i = 0; i < k; i++) arr[i] = Integer.parseInt(st.nextToken());
+				
+				int money = Integer.parseInt(st.nextToken());
+
+				reward.add(new Reward(money, arr));
+			}
+			
+			int[] sticker = new int[m];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < m; i++) sticker[i] = Integer.parseInt(st.nextToken());
+			long totalMoney = 0;
+			
+			while(!reward.isEmpty()) {
+				
+				Reward temp = reward.poll();
+				boolean flag = true;
+				while(true) {
+					
+					for(int i = 0; i < temp.need.length; i++) {
+						if(sticker[temp.need[i] - 1] < 1) {
+							flag = false;
+						}
+					}
+					if(!flag) break;
+					
+					for(int i = 0; i < temp.need.length; i++) {
+						sticker[temp.need[i] - 1]--;
+					}
+					
+					totalMoney += temp.money;
+					
+				}
+			}
+			System.out.println(totalMoney);
+		}
+		
+	}
+
+   // 17451번 - 평행 우주
+   public static void test05() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		long speed = arr[n - 1];
+		for(int i = n - 2; i >= 0; i--) {
+			
+			if(arr[i] > speed) speed = arr[i];
+			
+			else if(speed % arr[i] != 0) 
+				speed = (speed / arr[i] + 1) * arr[i];
+		}
+		
+		System.out.println(speed);
+	}
+	
+   // 11501번 - 주식 
+   public static void test06() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			
+			int N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			
+			long[] money = new long[N + 1];
+			for(int i = 0; i < N; i++) money[i] = Integer.parseInt(st.nextToken());
+			
+			long max = 0;
+			long gain = 0;
+			for(int i = N - 1; i >= 0; i--) {
+				if(money[i] > max) max = money[i];
+				else gain += (max - money[i]);
+			}
+			
+			sb.append(gain +"\n");
+		}
+		
+		sb.deleteCharAt(sb.length() - 1);
+		System.out.print(sb);
 	}
 
-
+   //
+   public static void test07() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+	}
+   
+   //
+   public static void test08() throws IOException {
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   StringTokenizer st;
+   }
 }
 
