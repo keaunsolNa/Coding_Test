2e67406fb6c3147b8090ddd4c2ecffc56e4f9df6
keaunsolNa <knsol1992@naver.com>
2023-04-23 19:19:11 +0900

    2023.04.23 DataStructure

--- CodingTest/src/baekjoon_DataStructure/DataStructure11.java
+++ CodingTest/src/baekjoon_DataStructure/DataStructure11.java
@@ -16,8 +16,26 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class DataStructure11 {
+	
+	private static class Tree implements Comparable<Tree> {
+		int x;
+		int y;
+		int z;
+
+		Tree(int x, int y, int z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		@Override
+		public int compareTo(Tree t) {
+			return this.z - t.z;
+		}
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		test08();
+		test10();
 	}
 	
 	// 27964번 - 콰트로치즈피자
@@ -218,7 +236,7 @@ public class DataStructure11 {
 		System.out.println(set.size());
 	}
 	
-	//
+	// 4649번 - Tanning Salon
 	public static void test08() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -231,20 +249,18 @@ public class DataStructure11 {
 			int size = Integer.parseInt(st.nextToken());
 			char[] line = st.nextToken().toCharArray();
 			
-			Set<Character> set = new HashSet<>();
-			int people = 0;
-			int answer = 0;
+			List<Character> list = new ArrayList<>();
+			Set<Character> ans = new HashSet<>();
 			for(int i = 0; i < line.length; i++) {
 				
-				if(set.contains(line[i])) { set.remove(line[i]); people--; }
-				else { set.add(line[i]); people++; };
-				
-				if(people > size) answer++;
+				if(!list.contains(line[i]) && list.size() < size) list.add(line[i]);
+				else if(list.contains(line[i])) list.remove((Object)line[i]); 
+				else if(!list.contains(line[i]) && list.size() >= size) ans.add(line[i]);
 				
 			}
 
-			if(answer == 0) sb.append("All customers tanned successfully.");
-			else sb.append(answer + " customer(s) walked away.");
+			if(ans.size() == 0) sb.append("All customers tanned successfully.");
+			else sb.append(ans.size() + " customer(s) walked away.");
 			
 			sb.append("\n");
 		}
@@ -252,18 +268,144 @@ public class DataStructure11 {
 		System.out.println(sb);
 	}
 	
-	//
+	// 16235번 - 나무 재테크
+	private static int N, M, K;
+	private static int[][] A;
+	private static int[][] map;
+	private static ArrayList<Tree> trees = new ArrayList<>();
+	private static ArrayList<Tree> liveTrees;
+	private static ArrayList<Tree> deadTrees;
+	private static int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	private static int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
 	public static void test09() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		map = new int[N][N];
+		A = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = 5;
+				A[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			trees.add(new Tree(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken())));
+		}
+
+		while (K > 0) {
+			liveTrees = new ArrayList<>();
+			deadTrees = new ArrayList<>();
+			Collections.sort(trees);
+			spring();
+			summer();
+			fall();
+			winter();
+			K--;
+		}
 		
+		System.out.println(trees.size());
 	}
 	
-	//
+	private static void spring() {
+		for (int i = 0; i < trees.size(); i++) {
+			Tree t = trees.get(i);
+			if (t.z > map[t.x][t.y]) {
+				deadTrees.add(t);
+			} else {
+				map[t.x][t.y] -= t.z;
+				t.z += 1;
+				liveTrees.add(t);
+			}
+		}
+		trees.clear();
+		trees.addAll(liveTrees);
+	}
+
+	private static void summer() {
+		for (int i = 0; i < deadTrees.size(); i++) {
+			Tree t = deadTrees.get(i);
+			map[t.x][t.y] += t.z / 2;
+		}
+	}
+
+	private static void fall() {
+		for (int i = 0; i < trees.size(); i++) {
+			Tree t = trees.get(i);
+			if (t.z % 5 == 0) {
+				for (int j = 0; j < 8; j++) {
+					int px = t.x + dx[j];
+					int py = t.y + dy[j];
+					if (0 <= px && px < N && 0 <= py && py < N) {
+						trees.add(new Tree(px, py, 1));
+					}
+				}
+			}
+		}
+	}
+
+	private static void winter() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] += A[i][j];
+			}
+		}
+	}
+	
+	// 17479번 - 정식당
 	public static void test10() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		Map<String, Integer> normal = new HashMap<String, Integer>();
+		for(int i = 0; i < A; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			normal.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+			
+		}
+		
+		Map<String, Integer> unique = new HashMap<String, Integer>();
+		for(int i = 0; i < B; i++) {
+			st = new StringTokenizer(br.readLine());
+			unique.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+		}
+		
+		List<String> special = new ArrayList<>();
+		for(int i = 0; i < C; i++) special.add(br.readLine());
+		
+		int order = Integer.parseInt(br.readLine());
+		
+		long normalOrder = 0;
+		long uniqueOrder = 0;
+		int specialOrderCount = 0;
+		
+		for(int i = 0; i < order; i++) {
+
+			String menu = br.readLine();
+			
+			if(normal.containsKey(menu)) normalOrder += normal.get(menu);
+			else if(unique.containsKey(menu)) uniqueOrder += unique.get(menu);
+			else specialOrderCount++;
+		}
+		
+		 if((normalOrder < 20000 && uniqueOrder > 0) || (normalOrder + uniqueOrder < 50000 && specialOrderCount > 0) || specialOrderCount > 1)
+	            System.out.println("No");
+	        else
+	            System.out.println("Okay");
+		 
 	}
 	
 	
