0d370715a892e518eaababdc8ea8c148be93c0c2
KeaunsolNa <knsol1992@naver.com>
2023-04-25 13:15:34 +0900

    2023.04.25 Sort

--- CodingTest/src/baekjoon_Sort/Sort07.java
+++ CodingTest/src/baekjoon_Sort/Sort07.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -64,8 +65,30 @@ public class Sort07 {
 		
 	}
 	
+	private static class Participant implements Comparable<Participant> {
+
+		int cls;
+		String name;
+		
+		Participant(int cls, String name) {
+			this.cls = cls;
+			this.name = name;
+		}
+		
+		@Override
+		public int compareTo(Participant o) {
+			
+			if(this.cls == o.cls) {
+				if(this.name.length() == o.name.length()) return this.name.compareTo(o.name);
+				else return this.name.length() - o.name.length();
+			}
+			return this.cls - o.cls;
+		}
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
-		test08();
+		test10();
 	}	
 	
 	// 13333번 - Q-인덱스
@@ -219,27 +242,212 @@ public class Sort07 {
 		
 	}
 	
-	// 
+	// 24174번 - 알고리즘 수업 - 힙 정렬 2
+	private static long K;
+	private static long cnt = 0;
+	private static boolean flag = false;
+	private static StringBuilder result = new StringBuilder();
 	public static void test08() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-	}
-
-	// 
-	public static void test09() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+		heapSort(arr);
 	}
 	
-	// 
+	private static void heapSort(int[] arr) {
+	    
+		int n = arr.length - 1;
+		buildMinHeap(arr, n);
+       
+		for (int i = n ; i >= 2; i--) {
+			swap(arr, 1, i);
+			heapify(arr, 1, i - 1);
+		}
+
+		if(flag) System.out.println(result);
+		else System.out.println(-1);
+
+	}
+
+    private static void buildMinHeap(int[] arr, int n) {
+        for (int i = n / 2; i >= 1; i--) {
+        	heapify(arr, i, n);
+        }
+    }
+    
+	private static void heapify(int[] arr, int e, int n) {
+		
+		int left = 2 * e;
+        int right = left + 1;
+        int smaller = 0;
+        
+        if (right <= n) {
+        	
+            if (arr[left] < arr[right]) smaller = left;
+            else smaller = right;
+            
+        } 
+        
+        else if (left <= n) smaller = left;
+        
+        else return;
+        
+        if (arr[smaller] < arr[e]) { 
+            swap(arr, e, smaller);
+            heapify(arr, smaller, n);
+        }
+	}
+	
+    private static void swap(int[] arr, int z, int x) {
+    	
+        cnt++;
+        int temp = arr[z];
+        arr[z] = arr[x];
+        arr[x] = temp;
+        
+        if(cnt == K) { 
+        	
+            flag = true;
+            for(int i = 1 ; i < arr.length ; i++) result.append(arr[i]+" ");
+            
+        }
+    }
+	
+	// 24173번 - 알고리즘 수업 - 힙 정렬 1
+	public static void test09() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		K = Long.parseLong(st.nextToken());
+		int[] arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+		heapSort2(arr);
+	}
+	
+
+	private static void heapSort2(int[] arr) {
+	    
+		int n = arr.length - 1;
+		buildMinHeap2(arr, n);
+       
+		for (int i = n ; i >= 2; i--) {
+			swap2(arr, 1, i);
+			heapify2(arr, 1, i - 1);
+		}
+
+		if(!flag) System.out.println(-1);
+
+	}
+
+    private static void buildMinHeap2(int[] arr, int n) {
+        for (int i = n / 2; i >= 1; i--) {
+        	heapify2(arr, i, n);
+        }
+    }
+    
+	private static void heapify2(int[] arr, int e, int n) {
+		
+		int left = 2 * e;
+        int right = left + 1;
+        int largest = 0;
+        
+        if (right <= n) {
+        	
+            if (arr[left] > arr[right]) largest = right;
+            else largest = left;
+            
+        } 
+        
+        else if (left <= n) largest = left;
+        
+        else return;
+        
+        if (arr[largest] < arr[e]) { 
+            swap2(arr, e, largest);
+            heapify2(arr, largest, n);
+        }
+	}
+	
+    private static void swap2(int[] arr, int z, int x) {
+    	
+        cnt++;
+        int temp = arr[z];
+        arr[z] = arr[x];
+        arr[x] = temp;
+        
+        if(cnt == K) { 
+        	
+        	if(arr[z] < arr[x]) System.out.print(arr[z] + " " + arr[x]);
+        	else System.out.println(arr[x] + " " + arr[z]);
+            System.exit(0);
+            
+        }
+    }
+	
+	// 23056번 - 참가자 명단
 	public static void test10() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		PriorityQueue<Participant> listOdd = new PriorityQueue<>();
+		PriorityQueue<Participant> listEven = new PriorityQueue<>();
+		
+		Map<Integer, Integer> list = new HashMap<>();
+		
+		while(true) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int cls = Integer.parseInt(st.nextToken());
+			String name = st.nextToken();
+			 
+			if(cls == 0 && name.equals("0")) break;
+			
+			if(!list.containsKey(cls)) list.put(cls, 1);
+			else if(list.get(cls) < M) list.put(cls, list.get(cls) + 1);
+			else continue;
+			
+			if(cls%2 == 0) listEven.add(new Participant(cls, name));
+			else listOdd.add(new Participant(cls, name));
+		}
+	
+		while(!listOdd.isEmpty()) {
+			
+			int cls = listOdd.peek().cls;
+			String name = listOdd.poll().name;
+			
+			System.out.println(cls + " " + name);
+		
+		}
+		
+		while(!listEven.isEmpty()) {
+			
+			int cls = listEven.peek().cls;
+			String name = listEven.poll().name;
+			
+			System.out.println(cls + " " + name);
+			
+		}
 	}
 	
 	
