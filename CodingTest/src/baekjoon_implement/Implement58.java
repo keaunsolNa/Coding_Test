3bd5963bdeef5e2cdca14b20380c7de7854f04de
KeaunsolNa <knsol1992@naver.com>
2023-06-20 20:59:32 +0900

    2023.06.20 Implement

--- CodingTest/src/baekjoon_implement/Implement58.java
+++ CodingTest/src/baekjoon_implement/Implement58.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
 public class Implement58 {
     
 	public static void main(String[] args) throws IOException {
-		test02();
+		test06();
 	}
 
 	
@@ -53,23 +53,160 @@ public class Implement58 {
 		}
 	}
 	
-	
-	// 
+	// 27621번 - Sum of Three Cubes
 	public static void test03() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long[][] a = {
+		     {0, 0, 0},
+		     {0, 0, 1},
+		     {0, 1, 1},
+		     {1, 1, 1},
+		     {0},
+		     {0},
+		     {-1, -1, 2},
+		     {0, -1, 2},
+		     {0, 0, 2},
+		     {0, 1, 2},
+		     {1, 1, 2},
+		     {-2, -2, 3},
+		     {7, 10, -11},
+		     {0},
+		     {0},
+		     {-1, 2, 2},
+		     {-511, -1609, 1626},
+		     {1, 2, 2},
+		     {-1, -2, 3},
+		     {0, -2, 3},
+		     {1, -2, 3},
+		     {-11, -14, 16},
+		     {0},
+		     {0},
+		     {-2901096694L, -15550555555L, 15584139827L},
+		     {-1, -1, 3},
+		     {0, -1, 3},
+		     {0, 0, 3},
+		     {0, 1, 3},
+		     {1, 1, 3},
+		     {-283059965, -2218888517L, 2220422932L},
+		     {0},
+		     {0},
+		     {8866128975287528L, -8778405442862239L, -2736111468807040L},
+		     {-1, 2, 3},
+		     {0, 2, 3},
+		     {1, 2, 3},
+		     {0, -3, 4},
+		     {1, -3, 4},
+		     {117367, 134476, -159380},
+		     {0},
+		     {0},
+		     {-80538738812075974L, 80435758145817515L, 12602123297335631L},
+		     {2, 2, 3},
+		     {-5, -7, 8},
+		     {2, -3, 4},
+		     {-2, 3, 3},
+		     {6, 7, -8},
+		     {-23, -26, 31},
+		     {0},
+		};
+				 
 		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < a[N].length; i++) sb.append(a[N][i] + " ");
+		
+		sb.deleteCharAt(sb.length() - 1);
+		
+		System.out.println(sb);
 		
 	}
 	
-	// 
+	// 18309번 - Extreme Temperatures
 	public static void test04() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		String input = "";
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		while((input = br.readLine()) != null) {
+			
+			st = new StringTokenizer(input);
+			String day = st.nextToken();
+			
+			while(st.hasMoreTokens()) {
+				
+				int temp = Integer.parseInt(st.nextToken());
+				min = Math.min(min, temp);
+				max = Math.max(max, temp);
+				
+			}
+			
+		}
+		System.out.println(min + " " + max);
+	}
+	
+	// 9806번 - The King
+	public static void test05() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(br.readLine());
+		int ans = 0;
+		int[] s = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+		for(int i = 0; i < N; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			s[i] = temp;
+		}
+		
+		if(A % 2 != 0) {
+			
+			for(int i = 0; i < N; i++) {
+				if(s[i] > 0) ans += Math.pow(s[i], A);
+			}
+		}
+		
+		else  {
+			for(int i = 0; i < N; i++) ans += Math.pow(s[i], A);
+		}
+		
+		System.out.println(ans);
+	}
+		
+	// 19786번 - Ставка
+	public static void test06() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		
+		while(t --> 0) {
+			
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int g = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int R = A * (((r + 1) * (r + 1)) + (g * g) + (b * b)) + C *(Math.min(r + 1, Math.min(g, b)));
+			int G = A * ((r * r) + ((g + 1) * (g + 1)) + (b * b)) + C *(Math.min(r, Math.min(g + 1, b)));
+			int B = A * ((r * r) + (g * g) + ((b + 1) * (b + 1))) + C *(Math.min(r, Math.min(g, b + 1)));
+			
+			if(Math.max(R, Math.max(G, B)) == R) System.out.println("RED");
+			else if(Math.max(R, Math.max(G, B)) == B) System.out.println("BLUE");
+			else System.out.println("GREEN");
+		}
 		
 	}
 	
 	// 
-	public static void test05() throws IOException {
+	public static void test07() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
