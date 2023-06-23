22a7247e50932932a8a5da641b6b558e0727e367
KeaunsolNa <knsol1992@naver.com>
2023-06-23 19:35:54 +0900

    2023.06.23 PrioirtyQueue

--- CodingTest/src/baekjoon_PriorityQueue/PriorityQueue03.java
+++ CodingTest/src/baekjoon_PriorityQueue/PriorityQueue03.java
@@ -3,8 +3,10 @@ package baekjoon_PriorityQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -12,7 +14,7 @@ import java.util.StringTokenizer;
 public class PriorityQueue03 {
 
 	public static void main(String[] args) throws IOException {
-		test03();
+		test04();
 	}
 	
 	private static class Pair implements Comparable<Pair> {
@@ -99,7 +101,6 @@ public class PriorityQueue03 {
         System.out.println(maximum);
 	}
 	
-	
 	// 12764번 - 싸지방에 간 준하
 	public static void test02() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -142,27 +143,90 @@ public class PriorityQueue03 {
         System.out.println(sb);
 	}
 	
-	
-	// 
+	// 2014번 - 소수의 곱
 	public static void test03() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 		
+		long[] primary = new long[K]; 
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < K; i++) {
+			primary[i] = Long.parseLong(st.nextToken());
+			pq.add(primary[i]);
+		}
+		
+		long head = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			head = pq.poll();
+			for(int j = 0; j < K; j++) {
+				long value = head * primary[j];
+				pq.add(value);
+				
+				if(head % primary[j] == 0) break;
+			}
+		}
+		
+		System.out.println(head);
 	}
 	
-	
+	// 11003번 - 최솟값 찾기
+	public static void test04() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		Deque<int[]> dq = new ArrayDeque<>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			
+			int Ai = Integer.parseInt(st.nextToken());
+			while(!dq.isEmpty() && dq.peekLast()[0] > Ai) dq.pollLast();
+			
+			dq.offer(new int[] {Ai, i} );
+			
+			if(dq.peek()[1] < i - (L - 1)) dq.poll();
+			
+			sb.append(dq.peek()[0] + " ");
+			
+		}
+		
+		System.out.print(sb);
+	}
 	
 	// 
-	public static void test04() throws IOException {
+	public static void test05() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 	}
 	
+	// 
+	public static void test06() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+	}
 	
 	// 
-	public static void test05() throws IOException {
+	public static void test07() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+	}
+	
+	// 
+	public static void test08() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
