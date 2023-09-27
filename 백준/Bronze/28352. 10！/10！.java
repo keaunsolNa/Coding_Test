a152e1ba89a96d6f5aaf5494a7702ded99178385
keaunsol Na <110284769+keaunsolNa@users.noreply.github.com>
2023-09-27 12:19:18 +0900

    [Bronze IV] Title: 10!, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/28352. 10！/10！.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        long second = factorial(N);
+        
+        System.out.println(second / 60 / 60 / 24 / 7);
+
+	}
+
+    private static long factorial(long N) {
+		
+	   long result = 1;
+
+	   for (long i = 1; i <= N; i++) {
+
+		   result = result * i;
+	   }
+	 
+	   return result;
+
+   }
+}
