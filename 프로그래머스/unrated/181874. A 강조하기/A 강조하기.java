8a178e611d3431839407c2017d14ffdf50411142
keaunsol Na <110284769+keaunsolNa@users.noreply.github.com>
2023-09-28 10:01:51 +0900

    [unrated] Title: A 강조하기, Time: 0.07 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        
+        myString = myString.toLowerCase();
+        myString = myString.replaceAll("a", "A");
+        
+        return myString;
+    }
+}
