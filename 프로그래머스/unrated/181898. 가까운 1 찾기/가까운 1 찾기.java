0672608383bacc94199456690e76ea59ac6b25a5
keaunsol Na <110284769+keaunsolNa@users.noreply.github.com>
2023-09-28 22:29:50 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        
+        for(int i = idx; i < arr.length; i++) {
+            if(arr[i] == 1) return i;
+        }
+        return -1;
+    }
+}
