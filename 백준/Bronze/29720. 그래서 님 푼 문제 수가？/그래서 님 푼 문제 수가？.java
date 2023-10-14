8448388e42e3985a7de901e284c38a481210ddf5
keaunsol Na <knsol1992@naver.com>
2023-10-14 14:29:33 +0900

    [Bronze III] Title: 그래서 님 푼 문제 수가?, Time: 144 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/29720. 그래서 님 푼 문제 수가？/그래서 님 푼 문제 수가？.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		System.out.print(Math.max(N - (M * K), 0));
+		System.out.println(" " + (N - (M * K) + (M -1)));
+   }
+
+}
