a43ab3389211ddcddedf3a007f403f15e020482a
keaunsol Na <knsol1992@naver.com>
2023-10-24 23:03:09 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(Integer.parseInt(st.nextToken()) * 2 >= Integer.parseInt(st.nextToken()) ? "E" : "H");
+	}
+}
