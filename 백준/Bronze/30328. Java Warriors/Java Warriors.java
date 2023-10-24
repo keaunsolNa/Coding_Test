057f69ab01307bb048826d409a38fcc387f58ca6
keaunsol Na <knsol1992@naver.com>
2023-10-24 23:00:32 +0900

    [Bronze V] Title: Java Warriors, Time: 120 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) * 4000);
+	}
+}
