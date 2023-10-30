33baff4b728c2fd159b99c1a37f0bb794d60e6bf
keaunsol Na <knsol1992@naver.com>
2023-10-31 08:48:11 +0900

    [Bronze III] Title: 암호 만들기, Time: 132 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String A = br.readLine();
+		String P = br.readLine();
+		
+		System.out.println(P);
+   }
+}
