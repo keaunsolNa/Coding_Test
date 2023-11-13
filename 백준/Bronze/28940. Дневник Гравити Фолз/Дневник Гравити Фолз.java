61db6161619e8e3543795f9b5843b0855713f2aa
keaunsol Na <knsol1992@naver.com>
2023-11-13 12:49:49 +0900

    [Bronze IV] Title: Дневник Гравити Фолз, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/28940. Дневник Гравити Фолз/Дневник Гравити Фолз.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int row = w / a;
+		int col = h / b;
+		
+		int page = row * col;
+		
+		if(page == 0) System.out.println(-1);
+		else System.out.println(n % page != 0 ? n / page + 1 : n / page);
+   }
+}
