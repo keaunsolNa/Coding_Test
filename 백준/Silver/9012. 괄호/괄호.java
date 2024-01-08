3ab40b90e74b8c0b79fb5b21a34de335e7965d58
keaunsol Na <knsol1992@naver.com>
2024-01-08 23:38:01 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,53 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			String s = br.readLine();
-			String answer = "NO";
-	        Deque<Character> deque = new LinkedList<>();
-	        char[] charArray = s.toCharArray();
-	        
-	        for (char c : charArray) {
-	        	if(c == '(') {
-	        		deque.add(c);
-	        	} else if(c == ')') {
-	        		if(deque.size() == 0) {
-	        			answer = "NO";
-	        			deque.add('N');
-	        			break;
-	        		} else {
-	        			deque.poll();
-	        		}
-	        	}
-			}
-	        
-	        if(deque.size() == 0) {
-	        	answer = "YES";
-	        }
-	        
-	        System.out.println(answer);
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        while(t --> 0) {
+
+            Stack<Character> stack = new Stack<>();
+            char[] charArray = br.readLine().toCharArray();
+
+            for (char c : charArray) {
+
+                // 문자열의 character 값이 '(' 라면 stack 에 넣는다.
+                if(c == '(') stack.add(c);
+
+                // 아니라면
+                else if(c == ')') {
+
+                    // stack 이 비었을 경우 올바르지 않은 문자열이다.
+                    // => 이전에 '('이 입력되지 않았으므로, ')'로 문자가 시작되는 케이스
+                    if(stack.isEmpty()) {
+
+                        // stack 이 비어있을 경우 올바른 문자열로 판별하므로 임의의 문자 'N'을 넣는다.
+                        stack.add('N');
+                        break;
+
+                    // 스택이 비어 있지 않다면 '()' 형태로 들어왔다는 의미이므로 기존의 '('를 pop 한다.
+                    } else stack.pop();
+
+                }
+            }
+
+            // 스택이 비어있다면 모든 문자열이 올바른 괄호 문자열이라는 의미.
+            sb.append(stack.isEmpty() ? "YES" : "NO").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
 }
