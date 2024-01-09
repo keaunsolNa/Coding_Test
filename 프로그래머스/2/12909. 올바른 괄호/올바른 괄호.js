482fe5e01dd5bbc2c055cbec3ede4bd45476c47d
keaunsol Na <knsol1992@naver.com>
2024-01-09 21:28:02 +0900

    [level 2] Title: 올바른 괄호, Time: 5.24 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,19 @@
 function solution(text){
-  const result = []
-  let answer = true
 
-  text.split('').map((obj) => {
-    if (obj === '(') {
-      result.push(obj)
-    } else {
-      if (result.length === 0) answer = false
-      result.pop()
+    let result = 0
+    let answer = true
+    
+    for (let obj of text) {
+      
+        if (obj === '(') {
+            result++
+        } else {
+            result--
+            if (result < 0) return answer = false
+        }
+
     }
-  })
 
-  return result.length === 0 && answer
+    return result === 0 && answer
+
 }
