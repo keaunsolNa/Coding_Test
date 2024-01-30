9b6047179e17d847eaf317171e8e6e0a612ffd64
keaunsol Na <knsol1992@naver.com>
2024-01-30 23:54:23 +0900

    [Silver V] Title: 알고리즘 수업 - 퀵 정렬 2, Time: 340 ms, Memory: 22308 KB -BaekjoonHub

--- 백준/Silver/24091. 알고리즘 수업 － 퀵 정렬 2/알고리즘 수업 － 퀵 정렬 2.java
+++ 백준/Silver/24091. 알고리즘 수업 － 퀵 정렬 2/알고리즘 수업 － 퀵 정렬 2.java
@@ -1,69 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int K;
-   public static void main(String[] args) throws IOException {
- 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    static StringBuffer sb = new StringBuffer();
+    static int[] A;
+    static long K;
+    static int size;
+    public static void quickSort(int[] A, int p, int r){
+       if(p < r){
+           int q = partition(A, p, r);
+           quickSort(A, p, q-1);
+           quickSort(A, q + 1, r);
+       }
+    }
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		quickSort2(arr, 0, N - 1);
-		
-		if(K > 0) {
-			System.out.println(-1);
-		} else {
-			for (int arr2 : arr) 
-				System.out.print(arr2 +" ");
-		}
-	}
-	
-	private static void quickSort2(int[] arr, int left, int right) {
+    public static int partition(int[] A, int p, int r){
+        int x = A[r];
+        int i = p - 1;
+        for(int j = p; j < r; j++){
+            if(A[j] <= x){
+                swap(++i,j);
+            }
 
-		if (left >= right) return; 
+        }
 
-	    int pivot = partition2(arr, left, right); 
+        if ( (i + 1) != r) {
+            swap(i+1,r);
+        }
 
-	    quickSort2(arr, left, pivot - 1); 
-	    quickSort2(arr, pivot + 1, right); 
-	}
+        return i + 1;
+    }
 
-	private static int partition2(int[] arr, int left, int right) {
-	    int pivot = arr[right]; 
-	    int i = left - 1;
-	    for (int j = left; j < right; j++) {
-	        if (arr[j] <= pivot) { 
-	        	++i; 
-	            swap2(arr, i, j); 
-	        }
-	    }
+    public static void swap(int i, int j){
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
 
-	    if(i + 1 != right) {
-	    	swap2(arr, i + 1, right); 
-	    }
-	    return i + 1; 
-	}
+        if(--K == 0){
+            for(int abc = 1; abc < size; abc++){
+                sb.append(A[abc]).append(" ");
+            }
+        }
+    }
 
-	private static void swap2(int[] arr, int i, int j) {
-		if(K == 0) {
-			for (int arr2 : arr) {
-				System.out.print(arr2 +" ");
-			}
-			System.exit(0);
-		}
-		K--;
-	    int temp = arr[i];
-	    arr[i] = arr[j];
-	    arr[j] = temp;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        size = Integer.parseInt(st.nextToken()) + 1;
+        K = Integer.parseInt(st.nextToken());
+        A = new int[size];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < size; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        quickSort(A, 1, size -1 );
+
+        if(sb.toString().isEmpty()){
+            System.out.print("-1");
+        } else{
+            System.out.print(sb.toString().replaceFirst(".$",""));
+        }
+    }
 }
