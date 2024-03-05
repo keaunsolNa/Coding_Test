9949f7fbf75dea141e5f2afbd5f1b9cd5285d3a4
keaunsol Na <knsol1992@naver.com>
2024-03-05 11:32:52 +0900

    [Silver V] Title: Trip Odometer, Time: 652 ms, Memory: 30012 KB -BaekjoonHub

---
+++ 백준/Silver/24936. Trip Odometer/Trip Odometer.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+        }
+
+        Set<Long> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(sum - arr[i]);
+        }
+
+        System.out.println(set.size());
+        for (long temp : set) {
+            System.out.print(temp + " ");
+        }
+
+
+    }
+
+
+
+}
