5bb49effa8b9206dc5ba428c0cc9634217be52f3
keaunsol Na <knsol1992@naver.com>
2024-03-14 10:31:01 +0900

    [Bronze IV] Title: 桁 (Digit), Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/31615. 桁 （Digit）/桁 （Digit）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(((a + b) + "").length());
+
+    }
+}
