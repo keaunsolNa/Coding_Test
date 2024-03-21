8a5ab912a09a42467e56bf19263c3f66e27da5ac
keaunsol Na <knsol1992@naver.com>
2024-03-21 16:07:36 +0900

    디렉토리 조정

--- CodingTest/src/Main.java
+++ CodingTest/src/Main.java
@@ -1,37 +1,71 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    private static final int MOD = 1000000007;
-    public static void main(String[] args) throws NumberFormatException, IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
 
-        int m = 4;
-        int n = 3;
-        int[][] puddles = new int[][]{{2, 2}};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] map = new int[n + 1][m + 1];
-        for (int i = 0; i < puddles.length; i++) map[puddles[i][1]][puddles[i][0]] = -1;
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
 
-        map[1][1] = 1;
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        int x = map.length;
-        int y = map[0].length;
-        for (int i = 1; i < n + 1; i++) {
+        Arrays.sort(arr);
 
-            for (int j = 1; j < m + 1; j++) {
+        System.out.println(solve(a, b, n, arr));
+    }
 
-                if (map[i][j] == -1) continue;
-                if (map[i - 1][j] > 0) map[i][j] += map[i - 1][j] % MOD;
-                if (map[i][j - 1] > 0) map[i][j] += map[i][j - 1] % MOD;
+    private static int solve(int a, int b, int n, int[] arr) {
+
+        int left = 0;
+        int right = 0;
+        boolean flag = true;
+
+        for (int i = 0; i < a; i++) {
+
+            b -= arr[i] / 2;
+            right = i + 1;
+
+            if (b < 0) {
+                flag = false;
+                return i;
             }
         }
 
+        if (flag) {
 
+            while (right < n) {
 
+                if (right - left < a) {
+
+                    b -= arr[right] / 2;
+                    if (b < 0) break;
+                    right++;
+                }
+
+                else {
+
+                    if (a > 0) b -= arr[left++] / 2;
+
+                    else {
+
+                        b -= arr[right];
+                        if (b < 0) break;
+                        right++;
+                    }
+                }
+            }
+
+            return right;
+        }
+
+        return right;
     }
-
 
 }
