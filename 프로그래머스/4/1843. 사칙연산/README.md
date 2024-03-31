049fe89e9b25a07e186200f5db8ee8937ba58c04
keaunsol Na <knsol1992@naver.com>
2024-03-31 16:41:01 +0900

    [level 4] Title: 사칙연산, Time: 15.76 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/README.md
+++ 프로그래머스/4/1843. 사칙연산/README.md
@@ -1,6 +1,6 @@
 # [level 4] 사칙연산 - 1843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1843?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1843) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 16:40:56
+2024년 03월 31일 16:40:59
 
 ### 문제 설명
 
--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,46 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(String arr[]) {
-        
-        int oc = (int) Math.ceil(arr.length / 2.0);
-        int[][] maxDp = new int[oc][oc];
-        int[][] minDp = new int[oc][oc];
-        
-         for (int i = 0; i < oc; i++) {
-
-            Arrays.fill(maxDp[i], Integer.MIN_VALUE);
-            Arrays.fill(minDp[i], Integer.MAX_VALUE);
-            maxDp[i][i] = Integer.parseInt(arr[i * 2]);
-            minDp[i][i] = Integer.parseInt(arr[i * 2]);
-             
-        }
-
-        for (int cnt = 1; cnt < oc; cnt++) 
-            
-            for (int i = 0; i < oc - cnt; i++) {
-                
-                int j = i + cnt;
-                
-                for (int k = i; k < j; k++) 
-                    
-                    if (arr[k * 2 + 1].equals("+")) {
-                        
-                        maxDp[i][j] = Math.max(maxDp[i][j], maxDp[i][k] + maxDp[k + 1][j]);
-                        minDp[i][j] = Math.min(minDp[i][j], minDp[i][k] + minDp[k + 1][j]);
-                        
-                    } else {
-                        
-                        maxDp[i][j] = Math.max(maxDp[i][j], maxDp[i][k] - minDp[k + 1][j]);
-                        minDp[i][j] = Math.min(minDp[i][j], minDp[i][k] - maxDp[k + 1][j]);
-                        
-                    }
-                
-            }
-        
-
-        return maxDp[0][oc - 1];
-        
+        int answer = -1;
+        return answer;
     }
 }
