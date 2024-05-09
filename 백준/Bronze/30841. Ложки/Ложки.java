904bd785d8bdf2fb00f69f3418354706bbe8c8b4
keaunsol Na <knsol1992@naver.com>
2024-05-10 06:47:56 +0900

    [Bronze III] Title: Ложки, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/30841. Ложки/Ложки.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if (a == 0 || b == 0) System.out.println(0);
+
+        else System.out.println(a * b / (a + b));
+
+    }
+}
