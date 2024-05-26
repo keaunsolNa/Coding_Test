9729489d360dac37c10866a518f6ec912085eaf3
keaunsol Na <knsol1992@naver.com>
2024-05-27 06:38:45 +0900

    [Bronze V] Title: 이 대회는 이제 제 겁니다, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(a + c, b));
+
+    }
+}
