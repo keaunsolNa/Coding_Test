7e47dbf8e48c954ac7f9160da51ba67c79d95229
keaunsol Na <knsol1992@naver.com>
2024-05-28 21:07:02 +0900

    [Silver I] Title: 밤양갱, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/31926. 밤양갱/밤양갱.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(10 + (int) Math.floor(Math.log(n) / Math.log(2)));
+    }
+}
