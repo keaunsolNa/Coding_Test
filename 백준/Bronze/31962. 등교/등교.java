59a6f8c748b0801e31557754101a7cdc20dee696
keaunsol Na <knsol1992@naver.com>
2024-06-18 21:43:50 +0900

    [Bronze IV] Title: 등교, Time: 124 ms, Memory: 14344 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31962. 등교/등교.java
+++ 백준/Bronze/31962. 등교/등교.java
@@ -12,8 +12,7 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-        int max = 0;
-        int ans = -1;
+        int max = -1;
         while (n --> 0) {
 
             st = new StringTokenizer(br.readLine());
@@ -22,11 +21,11 @@ public class Main {
             int t = Integer.parseInt(st.nextToken());
             int time = s + t;
 
-            if (time <= x && max < time) ans = s;
+            if (time <= x) max = Math.max(s, max);
 
         }
 
-        System.out.println(ans);
+        System.out.println(max);
     }
 
 }
