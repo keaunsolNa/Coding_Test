4fe0eef28e51fa7a029244d74fc89a6ef4e42f2c
keaunsol Na <knsol1992@naver.com>
2024-07-11 06:38:30 +0900

    [Bronze II] Title: 게임말 올려놓기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/31995. 게임말 올려놓기/게임말 올려놓기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        System.out.println(2 * (n - 1) * (m - 1));
+
+    }
+}
+
