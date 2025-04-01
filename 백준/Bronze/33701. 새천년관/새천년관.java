7a97050a87e655de04e2ccd73039d62fe31837d6
keaunsol Na <knsol1992@naver.com>
2025-04-02 08:04:27 +0900

    [Bronze IV] Title: 새천년관, Time: 92 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n, cnt;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("nukugwan");
+        bw.flush();
+        bw.close();
+    }
+
+}
