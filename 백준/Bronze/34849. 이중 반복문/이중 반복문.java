bf981c5f9bf364f3e6591ebb40ebb6de3acfca64
keaunsol Na <knsol1992@naver.com>
2025-12-01 11:23:14 +0900

    [Bronze V] Title: 이중 반복문, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        solve();
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void solve() throws IOException {
+
+        long n = Long.parseLong(br.readLine());
+        if (n * n <= 100000000) bw.write("Accepted");
+        else bw.write("Time limit exceeded");
+
+    }
+
+}
