78eb0227d8d9ead61f840688ae6bd3f28683c241
keaunsol Na <knsol1992@naver.com>
2025-12-03 15:03:57 +0900

    [Bronze IV] Title: 2, 4, 6, 8, Time: 96 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        solve();
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void solve() throws IOException {
+
+        bw.write("42");
+
+    }
+
+}
