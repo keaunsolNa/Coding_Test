7fa0a6e697b9f6eb343c4a5a4c49e80cde0e20f3
keaunsol Na <knsol1992@naver.com>
2025-12-05 10:06:12 +0900

    [Bronze III] Title: 얼룩말과 사자, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/34759. 얼룩말과 사자/얼룩말과 사자.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        solve();
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void solve() throws IOException {
+
+        long n = Long.parseLong(br.readLine());
+        bw.write(String.valueOf(n * 2));
+    }
+
+
+}
