6e4f3244b74721ebee9d175c7c2f21b0462bf9b2
keaunsol Na <knsol1992@naver.com>
2025-12-30 08:49:12 +0900

    [Bronze V] Title: 채플이 너무해, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        solve();
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void solve() throws IOException {
+
+        int x = Integer.parseInt(br.readLine());
+
+        bw.write(2 + x >= 8 ? "Success!" : "Oh My God!");
+
+
+    }
+
+
+}
