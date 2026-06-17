aab2ca8358f5ec6cdb8942380a9c6be59a3b8aba
keaunsol Na <knsol1992@naver.com>
2026-06-17 21:07:29 +0900

    feat: solved 0392-is-subsequence

--- leetcode/Solved/Easy/0392-is-subsequence/Solution.java
+++ leetcode/Solved/Easy/0392-is-subsequence/Solution.java
@@ -5,14 +5,11 @@ class Solution {
         int cnt = s.length();
         for (char c : s.toCharArray()) {
 
-            System.out.println("C : " + c);
             for (int i = index; i < t.length(); i++) {
                 
                 index++;
-                System.out.println(t.charAt(i) + " : " + c);
                 if (t.charAt(i) == c) {
                     cnt--;
-                    System.out.println(cnt);
                     break;
                 }                
             }
