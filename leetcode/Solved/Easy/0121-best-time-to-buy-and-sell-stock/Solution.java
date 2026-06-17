0d942b24a449da6ba0f0b14730febd903b2775c4
keaunsol Na <knsol1992@naver.com>
2026-06-17 20:59:57 +0900

    feat: solved 0121-best-time-to-buy-and-sell-stock

--- leetcode/Solved/Easy/0121-best-time-to-buy-and-sell-stock/Solution.java
+++ leetcode/Solved/Easy/0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -1,11 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
+        int target = prices[0];
         int max = 0;
-        for (int i = 0; i < prices.length; i++) {
-            
-            for (int j = i + 1; j < prices.length; j++) {
-                max = Math.max(max, prices[j] - prices[i]);
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < target) {
+                target = prices[i];
+            }            
+            else if (prices[i] - target > max) {
+                max = prices[i] - target;
             }
         }
 
