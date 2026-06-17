2a0c9ca9ce4c796a5cdc5730f94c3cecf6bf96f4
keaunsol Na <knsol1992@naver.com>
2026-06-17 21:05:26 +0900

    feat: solved 0392-is-subsequence

---
+++ leetcode/Solved/Easy/0392-is-subsequence/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int index = 0;
+        int cnt = s.length();
+        for (char c : s.toCharArray()) {
+
+            for (int i = index; i < t.length(); i++) {
+                
+                index++;
+                if (t.charAt(i) == c) {
+                    cnt++;
+                }                
+            }
+        }
+
+        return cnt == 0;
+    }
+}
