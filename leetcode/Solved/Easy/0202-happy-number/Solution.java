e2ae7ccb6d98600ef8ff5370a84b8949852eaf75
keaunsol Na <knsol1992@naver.com>
2026-07-13 10:17:27 +0900

    feat: solved 0202-happy-number

---
+++ leetcode/Solved/Easy/0202-happy-number/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(n, 0);
+
+        while (true) {
+
+            if (n == 1) return true;
+
+            if (map.containsKey(n)) return false;
+
+            String number = n + "";
+            int num = 0;
+            
+            for (int i = 0; i < number.length(); i++) {
+
+                int tn = Integer.parseInt(number.charAt(i) + "");
+                num += (tn * tn);
+            }
+
+            n = num;
+            map.put(n, 0);
+
+        }   
+    }
+}
