f252911afc790583365b40fafd5ea54dbb3c4960
keaunsol Na <knsol1992@naver.com>
2026-07-15 12:55:12 +0900

    feat: solved 0344-reverse-string

--- leetcode/Solved/Easy/0344-reverse-string/Solution.java
+++ leetcode/Solved/Easy/0344-reverse-string/Solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public void reverseString(char[] s) {
         
-        char[] reverse = new char[s.length];
+        char[] copy = Arrays.copyOf(s, s.length);
         int idx = 0;
 
-        for (int i = s.length - 1; i >= 0; i--) {
-            reverse[idx++] = s[i];
-            System.out.println(reverse[idx - 1] + " " + s[i]);            
+        for (int i = copy.length - 1; i >= 0; i--) {
+            s[idx++] = copy[i];
         }
 
-        for (int i = 0; i < s.length; i++) System.out.print(reverse[i]);
-
-        System.out.println();
-
-        for (int i = 0; i < s.length; i++) System.out.print(s[i]);
-        s = reverse;
     }
 }
