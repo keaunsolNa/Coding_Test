d760f3164364735b0d27f6d544d113b8e999ba6d
keaunsol Na <knsol1992@naver.com>
2026-07-16 13:58:37 +0900

    feat: solved 0771-jewels-and-stones

--- leetcode/Solved/Easy/0771-jewels-and-stones/Solution.java
+++ leetcode/Solved/Easy/0771-jewels-and-stones/Solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
 
-        int ans = 0;
-        char[] arr = jewels.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            
-            char target = arr[i];
-            ans += stones.chars().filter(c -> c == target).count();
+        int[] isJewel = new int[128];
+      
+        for (char jewel : jewels.toCharArray()) {
+            isJewel[jewel] = 1;
         }
-
-        return ans;   
+      
+        int jewelCount = 0;
+        for (char stone : stones.toCharArray()) {
+            jewelCount += isJewel[stone];
+        }
+      
+        return jewelCount;
     }
 }
