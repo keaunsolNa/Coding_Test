11987e2cf15cbf36d7b194cc395c65352bb6e214
keaunsol Na <knsol1992@naver.com>
2026-07-16 13:44:01 +0900

    feat: solved 1534-count-good-triplets

--- leetcode/Solved/Easy/1534-count-good-triplets/Solution.java
+++ leetcode/Solved/Easy/1534-count-good-triplets/Solution.java
@@ -5,9 +5,10 @@ class Solution {
         int ans = 0;
         for (int i = 0; i < arr.length; i++) {
 
-
             for (int j = i + 1; j < arr.length; j++) {
 
+                if (Math.abs(arr[i] - arr[j]) > a) continue;
+                
                 for (int k = j + 1; k < arr.length; k++) {
 
                     if (Math.abs(arr[i] - arr[j]) <= a &&
