422e1f5805c7a40ca6f566c6e33da713d1dd0eea
keaunsol Na <knsol1992@naver.com>
2026-07-16 14:58:16 +0900

    feat: solved 1356-sort-integers-by-the-number-of-1-bits

---
+++ leetcode/Solved/Easy/1356-sort-integers-by-the-number-of-1-bits/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            int count = Integer.bitCount(arr[i]);
+            map.put(arr[i], count);
+        }
+
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for (Integer key : keySet) {
+            answer[idx++] = key;
+        }
+
+        return answer;
+    }
+}
