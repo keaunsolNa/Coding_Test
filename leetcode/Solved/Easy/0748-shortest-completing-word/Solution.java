9ef0ff0129449545ec8d465aac67d7ad2609b40f
keaunsol Na <knsol1992@naver.com>
2026-07-21 15:26:16 +0900

    feat: solved 0748-shortest-completing-word

---
+++ leetcode/Solved/Easy/0748-shortest-completing-word/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        
+        String word = licensePlate.trim().replaceAll("[0-9]", "").toLowerCase();
+
+        char[] arr = word.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : arr) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (String str : words) {
+
+            char[] temp = str.toCharArray();
+            Map<Character, Integer> tempMap = new HashMap<>();
+
+            for (char c : temp) {
+
+                tempMap.put(c, tempMap.getOrDefault(c, 0) + 1);
+            }
+
+            boolean check = true;
+
+            for (char key : map.keySet()) {
+
+                if (tempMap.getOrDefault(key, 0) < map.get(key)) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) return str;
+            
+        }
+
+        return "";
+    }
+}
