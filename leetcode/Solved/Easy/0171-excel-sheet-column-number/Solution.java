ebb8bc1fbeed5ea414a178332d70e4f1a83e0873
keaunsol Na <knsol1992@naver.com>
2026-07-21 11:12:13 +0900

    feat: solved 0171-excel-sheet-column-number

--- leetcode/Solved/Easy/0171-excel-sheet-column-number/Solution.java
+++ leetcode/Solved/Easy/0171-excel-sheet-column-number/Solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
         
-        char[] arr = columnTitle.toCharArray();
         int ans = 0;
 
-        for (int i = arr.length - 1; i >= 0; i--) {
+        for (int i = 0; i < columnTitle.length(); i++) {
 
-            int idx = arr.length - i - 1;
-            if (idx > 0) ans += 26 * (arr[i] - 'A' + idx);
-            else ans += arr[i] - 'A' + 1;
+            char currentChar = columnTitle.charAt(i);
+          
+            int charValue = currentChar - 'A' + 1;
+          
+            ans = ans * 26 + charValue;
         }
 
         return ans;
