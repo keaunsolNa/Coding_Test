fa0b109b07b63977b2d3dc231d8636c93644aeed
keaunsol Na <knsol1992@naver.com>
2026-07-21 14:08:55 +0900

    feat: solved 0415-add-strings

--- leetcode/Solved/Easy/0415-add-strings/Solution.java
+++ leetcode/Solved/Easy/0415-add-strings/Solution.java
@@ -1,8 +1,26 @@
 class Solution {
     public String addStrings(String num1, String num2) {
 
-        int num = Integer.parseInt(num1) + Integer.parseInt(num2);
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
 
-        return String.valueOf(num);
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+
+            int digit1 = (i < 0) ? 0 : num1.charAt(i) - '0';
+            int digit2 = (j < 0) ? 0 : num2.charAt(j) - '0';
+          
+            carry += digit1 + digit2;
+            sb.append(carry % 10);
+            carry /= 10;
+          
+            i--;
+            j--;
+        }
+
+
+        return sb.reverse().toString();
     }
 }
