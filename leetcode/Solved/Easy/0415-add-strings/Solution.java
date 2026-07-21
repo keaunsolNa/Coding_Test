dff6b615907cba1bf800d228febeb4828f836653
keaunsol Na <knsol1992@naver.com>
2026-07-21 14:03:10 +0900

    feat: solved 0415-add-strings

---
+++ leetcode/Solved/Easy/0415-add-strings/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+
+        int num = Integer.parseInt(num1) + Integer.parseInt(num2);
+
+        return String.valueOf(num);
+    }
+}
