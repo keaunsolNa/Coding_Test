4acda64e7cc567fd3d24e9d94a8f4546a57ee3a4
keaunsol Na <knsol1992@naver.com>
2026-07-21 11:06:36 +0900

    feat: solved 0171-excel-sheet-column-number

--- leetcode/Solved/Easy/0171-excel-sheet-column-number/Solution.java
+++ leetcode/Solved/Easy/0171-excel-sheet-column-number/Solution.java
@@ -8,16 +8,8 @@ class Solution {
 
             int idx = arr.length - i - 1;
 
-            System.out.println("idx : " + idx);
-
-            if (idx > 0) {
-                ans += 26 * (arr[i] - 'A' + idx);
-            }
+            if (idx > 0) ans += 26 * (arr[i] - 'A' + idx);
             else ans += arr[i] - 'A' + 1;
-            // System.out.println(Math.pow(arr[i] - 'A' + 1, idx == 0 ? 1 : idx + 1));
-            // target += Math.pow(arr[i] - 'A' + 1, idx == 0 ? 1 : idx + 1);
-            // ans += Math.pow(arr[i] - 'A' + 1, idx == 0 ? 1 : idx + 1);
-                // System.out.println(target);
         }
 
         return ans;
