1647a0b6430231c94a4aa247ab627e3ed69314de
keaunsol Na <knsol1992@naver.com>
2026-07-22 15:32:23 +0900

    feat: solved 0645-set-mismatch

--- leetcode/Solved/Easy/0645-set-mismatch/Solution.java
+++ leetcode/Solved/Easy/0645-set-mismatch/Solution.java
@@ -10,14 +10,14 @@ class Solution {
         System.out.println(min + "   " + max);
 
         for (int i = 0; i < nums.length; i++) {
-            arr[nums[i] - 1]++;
+            arr[nums[i]]++;
         }       
 
         for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
         System.out.println();
         
         int[] ans = new int[2];
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] == 0) ans[1] = i;
             if (arr[i] == 2) ans[0] = i;
