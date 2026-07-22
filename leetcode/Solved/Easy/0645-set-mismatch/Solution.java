36c3a20c36b93393cdd4afcac259bafbceea5105
keaunsol Na <knsol1992@naver.com>
2026-07-22 15:27:28 +0900

    feat: solved 0645-set-mismatch

---
+++ leetcode/Solved/Easy/0645-set-mismatch/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int min = 1;
+        int max = nums[nums.length - 1];
+        int[] arr = new int[max + 1];
+
+        for (int i = 0; i < max; i++) {
+            arr[nums[i]]++;
+        }       
+
+
+        int[] ans = new int[2];
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] == 0) ans[1] = i;
+            if (arr[i] == 2) ans[0] = i;
+        }
+
+        return ans;
+    }
+}
