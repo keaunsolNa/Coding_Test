332fde6a7a2295fde5092fd79d69ef2a03e919c9
keaunsol Na <knsol1992@naver.com>
2026-07-22 16:53:04 +0900

    feat: solved 0812-largest-triangle-area

--- leetcode/Solved/Easy/0812-largest-triangle-area/Solution.java
+++ leetcode/Solved/Easy/0812-largest-triangle-area/Solution.java
@@ -5,6 +5,7 @@ class Solution {
         int maxY = 0;
         int minX = 99999999;
         int minY = 99999999;
+        
         for (int i = 0; i < points.length; i++) {
 
             maxX = Math.max(points[i][0], maxX);        
@@ -15,13 +16,8 @@ class Solution {
 
         double x = Math.abs(maxX - minX);
         double y = Math.abs(maxY - minY);
+        double z = x * y / 2;
 
-        System.out.println(x + "  " + y);
-        double z = x * y;
-        
-        System.out.println(z);
-        z /= 2;
-        System.out.println(z);
         return (Math.round(z*1000)/1000.0);
     }
 }
