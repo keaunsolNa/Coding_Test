05ea08cb23563c77c100f781663a9ad1c3fbe097
keaunsol Na <knsol1992@naver.com>
2026-07-22 10:41:30 +0900

    feat: solved 0796-rotate-string

--- leetcode/Solved/Easy/0796-rotate-string/Solution.java
+++ leetcode/Solved/Easy/0796-rotate-string/Solution.java
@@ -29,27 +29,19 @@ class Solution {
 
             if (check) return check;
             
-            for (char c : sArr) System.out.print(c);
-            System.out.println();
+            temp[sArr.length - 2] = sArr[sArr.length - 1];
+            for (int i = 1; i < sArr.length; i++) {
+                temp[i - 1] = sArr[i];
+            }
 
-            for (char c : temp) System.out.print(c);
-            System.out.println();
+            for (int i = 0; i < sArr.length; i++) sArr[i] = temp[i];
 
-            break;
-            // temp[sArr.length - 2] = sArr[sArr.length - 1];
-            // for (int i = 1; i < sArr.length; i++) {
-            //     temp[i - 1] = sArr[i];
-            // }
+            loop++;
 
-            // for (int i = 0; i < sArr.length; i++) sArr[i] = temp[i];
-
-            // loop++;
-
-            // if (loop > goal.length()) return false;
+            if (loop > goal.length()) return false;
 
 
         }
 
-        return false;
     }
 }
