62acc78d297a7140e2fc1a158a2c2bc42050b4f6
keaunsol Na <knsol1992@naver.com>
2026-07-22 16:55:23 +0900

    feat: solved 0812-largest-triangle-area

--- leetcode/Solved/Easy/0812-largest-triangle-area/Solution.java
+++ leetcode/Solved/Easy/0812-largest-triangle-area/Solution.java
@@ -1,23 +1,32 @@
 class Solution {
     public double largestTriangleArea(int[][] points) {
         
-        int maxX = 0;
-        int maxY = 0;
-        int minX = 99999999;
-        int minY = 99999999;
-        
-        for (int i = 0; i < points.length; i++) {
-
-            maxX = Math.max(points[i][0], maxX);        
-            maxY = Math.max(points[i][1], maxY);
-            minX = Math.min(points[i][0], minX);
-            minY = Math.min(points[i][1], minY);
+        double maxArea = 0;
+      
+        for (int[] point1 : points) {
+            int x1 = point1[0];
+            int y1 = point1[1];
+          
+            for (int[] point2 : points) {
+                int x2 = point2[0];
+                int y2 = point2[1];
+              
+                for (int[] point3 : points) {
+                    int x3 = point3[0];
+                    int y3 = point3[1];
+                  
+                    int vectorX1 = x2 - x1;
+                    int vectorY1 = y2 - y1;
+                    int vectorX2 = x3 - x1;  
+                    int vectorY2 = y3 - y1;  
+                  
+                    double currentArea = Math.abs(vectorX1 * vectorY2 - vectorX2 * vectorY1) / 2.0;
+                  
+                    maxArea = Math.max(maxArea, currentArea);
+                }
+            }
         }
-
-        double x = Math.abs(maxX - minX);
-        double y = Math.abs(maxY - minY);
-        double z = x * y / 2;
-
-        return (Math.round(z*1000)/1000.0);
+      
+        return maxArea;
     }
 }
