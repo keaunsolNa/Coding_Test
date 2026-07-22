96d972bf9e5bcab2eea207f0c94a008b414a170e
keaunsol Na <knsol1992@naver.com>
2026-07-22 11:06:27 +0900

    feat: solved 0088-merge-sorted-array

--- leetcode/Solved/Easy/0088-merge-sorted-array/Solution.java
+++ leetcode/Solved/Easy/0088-merge-sorted-array/Solution.java
@@ -2,14 +2,8 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
         int idx = 0;
-        if (n == 0) return;
-        if (m == 0) 
-        {
-            nums1[0] = nums2[0];
-            nums2[0] = 0;
-            return;
-        }
-        for (int i = n; i < m + n; i++) {
+
+        for (int i = m; i < m + n; i++) {
             nums1[i] = nums2[idx];
             nums2[idx] = 0;
             idx++;
