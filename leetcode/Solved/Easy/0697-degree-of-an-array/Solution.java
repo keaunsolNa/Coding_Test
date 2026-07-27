badd578a13464b2264af6aaaaa5c42048f93d6d9
keaunsol Na <knsol1992@naver.com>
2026-07-27 10:07:51 +0900

    feat: solved 0697-degree-of-an-array

---
+++ leetcode/Solved/Easy/0697-degree-of-an-array/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxCnt = 0;
+        int target = 0;
+        int startIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+            if (maxCnt < map.get(nums[i])) {
+
+                maxCnt = map.get(nums[i]);
+                target = nums[i];
+                maxIndex = i;
+            }
+
+        }   
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == target) {
+                startIndex = i;
+                break;
+            }
+        }
+
+        return maxIndex - startIndex;
+
+    }
+}
