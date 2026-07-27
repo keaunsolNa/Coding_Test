1d860bb4bf523c28e86821ffc67f99ae7531f771
keaunsol Na <knsol1992@naver.com>
2026-07-27 17:20:10 +0900

    feat: solved 1399-count-largest-group

---
+++ leetcode/Solved/Easy/1399-count-largest-group/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countLargestGroup(int n) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+
+            int mod = 10;
+            int num = 0;
+            int origin = i;
+
+            while(origin > 0) {
+
+                num += origin % mod;
+                origin -= origin % mod;
+                mod *= 10;
+
+            }
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        }
+
+        int ans = 0;
+
+        for (int key : map.keySet()) {
+
+            ans = Math.max(ans, map.get(key));
+        }
+
+        return ans;
+    }
+}
