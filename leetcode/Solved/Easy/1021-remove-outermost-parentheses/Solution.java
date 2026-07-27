d6799caa2d6a82eabdc72400c02a41d39011b7ef
keaunsol Na <knsol1992@naver.com>
2026-07-27 15:47:45 +0900

    feat: solved 1021-remove-outermost-parentheses

---
+++ leetcode/Solved/Easy/1021-remove-outermost-parentheses/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            
+            
+            if (c == '(') {
+                sb.append(c);
+            }
+            else {
+
+                sb.append(c);
+                list.add(sb.toString());
+                sb.setLength(0);
+            }
+        }
+
+        sb.setLength(0);
+
+        for (String str : list) {
+            
+            sb.append(str.replaceAll("^.|.$", ""));
+        }
+
+        return sb.toString();
+    }
+}
