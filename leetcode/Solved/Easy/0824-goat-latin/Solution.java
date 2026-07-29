f19ea3c7def5c16840b243fc64d546ecb91fc067
keaunsol Na <knsol1992@naver.com>
2026-07-29 16:29:13 +0900

    feat: solved 0824-goat-latin

---
+++ leetcode/Solved/Easy/0824-goat-latin/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        
+        String[] st = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < st.length; i++) {
+
+            StringBuilder target = new StringBuilder(st[i]);
+            char c = target.toString().charAt(0);
+            
+            switch (c) {
+                case 'a' :
+                case 'e' :
+                case 'i' :
+                case 'o' :
+                case 'u' :
+                    target.append("ma");
+                    break;
+
+                default : 
+                    
+                    target.delete(0, 1);
+                    target.append(c);
+                    target.append("ma");
+            }
+
+            for (int j = 0; j < i + 1; j++) target.append("a");
+
+            ans.append(target);
+            if (i < st.length) ans.append(" ");
+        }
+
+        return ans.toString();
+
+
+    }
+}
