2d103f68d7716b4e89865468091c05833ff0d1e3
keaunsol Na <knsol1992@naver.com>
2026-07-30 15:18:12 +0900

    feat: solved 0007-reverse-integer

--- leetcode/Solved/Med/0007-reverse-integer/Solution.java
+++ leetcode/Solved/Med/0007-reverse-integer/Solution.java
@@ -1,15 +1,24 @@
 class Solution {
     public int reverse(int x) {
         
-        StringBuilder sb = new StringBuilder();
-        boolean isMinus = false;
+    int rev = 0;
 
-        for (char c : String.valueOf(x).toCharArray()) {
+    while (x != 0) {
+        
+        int pop = x % 10;
+        x /= 10;
 
-            if (c == '-') isMinus = true;
-            else sb.insert(0, c);
-        }
+        if (rev > Integer.MAX_VALUE / 10 ||
+            (rev == Integer.MAX_VALUE / 10 && pop > 7))
+            return 0;
 
-        return Integer.parseInt(sb.toString()) * (isMinus ? -1 : 1);
+        if (rev < Integer.MIN_VALUE / 10 ||
+            (rev == Integer.MIN_VALUE / 10 && pop < -8))
+            return 0;
+
+        rev = rev * 10 + pop;
+    }
+
+    return rev;
     }
 }
