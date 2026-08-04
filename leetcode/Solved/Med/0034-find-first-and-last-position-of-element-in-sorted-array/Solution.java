b6cc0e23d288081c225a5418cd28af4b4acd0804
keaunsol Na <knsol1992@naver.com>
2026-08-04 17:07:38 +0900

    feat: solved 0034-find-first-and-last-position-of-element-in-sorted-array

--- leetcode/Solved/Med/0034-find-first-and-last-position-of-element-in-sorted-array/Solution.java
+++ leetcode/Solved/Med/0034-find-first-and-last-position-of-element-in-sorted-array/Solution.java
@@ -1,27 +1,53 @@
 class Solution {
+    private int[] nums;
+    private int n;
+
     public int[] searchRange(int[] nums, int target) {
-        
-        int firstIndex = Integer.MAX_VALUE;
-        int lastIndex = 0;
-        boolean check = false;
+        this.nums = nums;
+        this.n = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
+        if (n == 0) {
+            return new int[] {-1, -1};
+        }
 
-            if (nums[i] == target) {
-                check = true;
-                firstIndex = Math.min(i, firstIndex);
-                lastIndex = Math.max(i, lastIndex);
+        int firstIdx = findFirstTrue(target, false);
+        if (firstIdx == -1 || nums[firstIdx] != target) {
+            return new int[] {-1, -1};
+        }
+
+        int afterLastIdx = findFirstTrue(target, true);
+        int lastIdx;
+        if (afterLastIdx == -1) {
+            lastIdx = n - 1;
+        } else {
+            lastIdx = afterLastIdx - 1;
+        }
+
+        return new int[] {firstIdx, lastIdx};
+    }
+
+    private int findFirstTrue(int target, boolean findGreater) {
+        int left = 0;
+        int right = n - 1;
+        int firstTrueIndex = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            boolean feasible;
+            if (findGreater) {
+                feasible = nums[mid] > target;
+            } else {
+                feasible = nums[mid] >= target;
+            }
+
+            if (feasible) {
+                firstTrueIndex = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
-        int[] arr = new int[] {-1 ,-1};
-        if (!check) return arr;
-        else {
-
-            arr[0] = firstIndex;
-            arr[1] = lastIndex;
-
-            return arr;
-        }
+        return firstTrueIndex;
     }
 }
