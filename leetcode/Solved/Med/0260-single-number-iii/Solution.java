4f9eaa2a66a976586446db3a9db9d6830121175f
keaunsol Na <knsol1992@naver.com>
2026-08-04 15:58:18 +0900

    feat: solved 0260-single-number-iii

--- leetcode/Solved/Med/0260-single-number-iii/Solution.java
+++ leetcode/Solved/Med/0260-single-number-iii/Solution.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
 
-            if (list.contains(nums[i])) list.remove(Integer.valueOf(i));
+            if (list.contains(nums[i])) list.remove(Integer.valueOf(nums[i]));
             else list.add(nums[i]);
 
             System.out.println(list);
@@ -14,8 +14,8 @@ class Solution {
         System.out.println(list);
 
         int[] answer = new int[2];
-        // answer[0] = list.get(0);
-        // answer[1] = list.get(1);
+        answer[0] = list.get(0);
+        answer[1] = list.get(1);
 
         return answer;
     }
