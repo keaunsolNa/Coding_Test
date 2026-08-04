67c09ef5e1b61d85501b08c0908ea3dc5e70d991
keaunsol Na <knsol1992@naver.com>
2026-08-04 17:53:59 +0900

    feat: solved 1475-final-prices-with-a-special-discount-in-a-shop

--- leetcode/Solved/Easy/1475-final-prices-with-a-special-discount-in-a-shop/Solution.java
+++ leetcode/Solved/Easy/1475-final-prices-with-a-special-discount-in-a-shop/Solution.java
@@ -10,7 +10,7 @@ class Solution {
 
             for (int j = i + 1; j < prices.length; j++) {
 
-                if (price > prices[j]) {
+                if (price >= prices[j]) {
                     discount = prices[j];
                     break;
                 }
