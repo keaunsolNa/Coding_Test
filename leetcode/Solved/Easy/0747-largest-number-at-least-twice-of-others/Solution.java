0976fd5442bac4c1ccff059a89a862287706b89f
keaunsol Na <knsol1992@naver.com>
2026-08-04 16:27:00 +0900

    feat: solved 0747-largest-number-at-least-twice-of-others

--- leetcode/Solved/Easy/0747-largest-number-at-least-twice-of-others/Solution.java
+++ leetcode/Solved/Easy/0747-largest-number-at-least-twice-of-others/Solution.java
@@ -8,18 +8,18 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
 
             int target = nums[i];
-            System.out.print(target + " ");
 
             if (max < target) {
 
-                secondMax = max;
                 max = target;
                 idx = i;
             }
         }
 
-        System.out.println();
-        
+        Arrays.sort(nums);
+        secondMax = nums[nums.length - 2];
+
+
         System.out.println(max + " " + secondMax + " " + idx);
 
         if (max >= secondMax * 2) return idx;
