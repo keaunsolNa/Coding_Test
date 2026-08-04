09c055bb0e57ef89caec2379215caf1d0825a388
keaunsol Na <knsol1992@naver.com>
2026-08-05 08:59:07 +0900

    feat: solved 1002-find-common-characters

---
+++ leetcode/Solved/Easy/1002-find-common-characters/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+
+        int[] arr = new int[26];
+        Arrays.fill(arr, 101);
+
+        for (String str : words) {
+    
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (char c : str.toCharArray()) {
+
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+
+            for (char c : map.keySet()) {
+                arr[c - 'a'] = Math.min(map.get(c), arr[c - 'a']);
+            }
+
+        }
+
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+
+            if (arr[i] > 0) {
+                
+                String target = String.valueOf(arr[i] + 'a');
+                for (int j = 0; j < arr[i]; j++) answer.add(target);
+            }
+        }
+
+        return answer;
+
+    }
+}
