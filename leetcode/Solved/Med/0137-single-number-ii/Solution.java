d8ec03e6084b6f18c74bd7f7d892bc8f5ea37f60
keaunsol Na <knsol1992@naver.com>
2026-08-04 16:06:32 +0900

    feat: solved 0137-single-number-ii

--- leetcode/Solved/Med/0137-single-number-ii/Solution.java
+++ leetcode/Solved/Med/0137-single-number-ii/Solution.java
@@ -1,17 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-        int[] arr = new int[3 * 10000 + 1];
-        int ans = 0;
-        
-        for (int x : nums) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-            arr[x]++;
-            if (arr[x] == 3) ans ^= x;
-            
+        for (int i = 0; i < nums.length; i++) {
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+            if (map.get(nums[i]) == 3) map.remove(nums[i]);
         }
-        
-        return ans;
 
+        int ans = 0;
+        for (int key : map.keySet()) {
+            ans = key;
+        }
+
+        return ans;
     }
 }
