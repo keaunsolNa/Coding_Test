484ebeb1a8c233a2ba81826eb9e247b753ba025e
keaunsol Na <knsol1992@naver.com>
2026-08-05 09:17:12 +0900

    feat: solved 1002-find-common-characters

--- leetcode/Solved/Easy/1002-find-common-characters/Solution.java
+++ leetcode/Solved/Easy/1002-find-common-characters/Solution.java
@@ -32,7 +32,7 @@ class Solution {
 
             if (arr[i] > 0) {
                 
-                char target = (char)(arr[i] + 'a');
+                char target = (char)(arr[i] + 26);
                 for (int j = 0; j < arr[i]; j++) answer.add(String.valueOf(target));
             }
         }
