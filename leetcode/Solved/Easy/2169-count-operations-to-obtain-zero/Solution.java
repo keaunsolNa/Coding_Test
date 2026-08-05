da3b3d55fa988102b6a99c32cf68637eff47501c
keaunsol Na <knsol1992@naver.com>
2026-08-05 17:41:52 +0900

    feat: solved 2169-count-operations-to-obtain-zero

--- leetcode/Solved/Easy/2169-count-operations-to-obtain-zero/Solution.java
+++ leetcode/Solved/Easy/2169-count-operations-to-obtain-zero/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         
         int ans = 0;
 
-        while (num1 == 0 || num2 == 0) {
+        while (num1 != 0 && num2 != 0) {
 
             if (num1 > num2) num1 -= num2;
             else if (num1 < num2) num2 -= num1;
