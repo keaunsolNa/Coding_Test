a75dc924a1350b432bca285d8dc52865713cdb7c
keaunsol Na <knsol1992@naver.com>
2026-08-06 10:57:46 +0900

    feat: solved 3345-smallest-divisible-digit-product-i

---
+++ leetcode/Solved/Easy/3345-smallest-divisible-digit-product-i/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+        while (true) {
+            
+            int product = 1;
+            
+            int temp = n;
+            while (temp > 0) {
+                int digit = temp % 10;
+                product *= digit; 
+                temp /= 10;            
+            }
+
+            System.out.println(product + " " + temp);
+
+            if (product > t) return product;
+            else n++;
+
+            return 0;
+        }       
+    }
+}
