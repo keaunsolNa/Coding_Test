645ab25634524e408e3424aaf43db6d59da47f5c
keaunsol Na <knsol1992@naver.com>
2026-08-11 15:03:19 +0900

    feat: solved 0944-delete-columns-to-make-sorted

--- leetcode/Solved/Easy/0944-delete-columns-to-make-sorted/Solution.java
+++ leetcode/Solved/Easy/0944-delete-columns-to-make-sorted/Solution.java
@@ -1,38 +1,45 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
 
+        int l = strs.length;
         int n = strs[0].length();
 
         if (n == 1) return 0;
 
         int ans = 0;
+        int[][] arr = new int[l][n];
 
-        for (String str : strs) {
+        for (int i = 0; i < l; i++) {
             
-            int prev = str.charAt(0) - 'a';
-            int now = str.charAt(1) - 'a';
+            for (int j = 0; j < n; j++) {
+
+                arr [j][i] = (int) strs[i].charAt(j) - 'a';
+            }
+
+        }
+
+        for (int i = 0; i < l; i++) {
+            
+            int prev = arr[i][0];
+            int now = arr[i][1];
             boolean asc = prev <= now;
 
             prev = now;
 
-            for (int i = 2; i < str.length(); i++) {
-                
-                now = str.charAt(i) - 'a';
+            for (int j = 2; j < n; j++) {
 
-                if (asc && prev > now) {
+                if (asc && prev <= now ) {
                     ans++;
                     break;
                 }
 
-                else if (!asc && prev < now) {
+                else if (!asc && prev >= now) {
                     ans++;
                     break;
                 }
-
-                prev = now;
             }
-        }       
-
+        }
+        
         return ans;
     }
 }
