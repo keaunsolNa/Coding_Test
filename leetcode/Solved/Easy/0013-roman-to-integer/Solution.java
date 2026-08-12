0e7f22bbffed5c8be024b8e3c9749805dce8c65c
keaunsol Na <knsol1992@naver.com>
2026-08-12 11:06:09 +0900

    feat: solved 0013-roman-to-integer

---
+++ leetcode/Solved/Easy/0013-roman-to-integer/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        char[] arr = s.toCharArray();
+        
+        int prev = convertCharToInt(arr[0]);
+        int total = prev;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            char c = arr[i];
+            int now = convertCharToInt(c);
+
+            if (now > prev) total -= now;
+            else total += now;
+
+            prev = now;
+        }
+
+        return total;
+    }
+
+    private int convertCharToInt(char c)  {
+
+        int num = switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+
+        return num;
+    }
+}
