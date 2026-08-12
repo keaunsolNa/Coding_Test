59ff4249d7535f8ce4da82b9fceafa35bdf03db7
keaunsol Na <knsol1992@naver.com>
2026-08-12 16:06:30 +0900

    feat: solved 0925-long-pressed-name

---
+++ leetcode/Solved/Easy/0925-long-pressed-name/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        
+        char[] nameArr = name.toCharArray();
+        char[] typedArr = typed.toCharArray();
+
+        Map<Character, Integer> nameMap = new HashMap<>();
+        Map<Character, Integer> typedMap = new HashMap<>();
+
+        for (char c : nameArr) nameMap.put(c, nameMap.getOrDefault(c, 0) + 1);
+
+        for (char c : typedArr) typedMap.put(c, typedMap.getOrDefault(c, 0) + 1);
+
+        for (char c : nameMap.keySet()) {
+
+            if (typedMap.get(c) < nameMap.get(c)) return false;
+        }
+
+        return true;
+        
+    }
+}
