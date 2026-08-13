a0ee03b5487028d717389fa398b4ac4cacf12d62
keaunsol Na <knsol1992@naver.com>
2026-08-13 14:20:10 +0900

    feat: solved 0225-implement-stack-using-queues

---
+++ leetcode/Solved/Easy/0225-implement-stack-using-queues/Solution.java
@@ -0,0 +1,43 @@
+class MyStack {
+
+    private Deque<Integer> primaryQueue = new ArrayDeque<>();
+    private Deque<Integer> auxiliaryQueue = new ArrayDeque<>();
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+
+        auxiliaryQueue.offer(x);
+      
+        while (!primaryQueue.isEmpty()) {
+            auxiliaryQueue.offer(primaryQueue.poll());
+        }
+      
+        Deque<Integer> temp = primaryQueue;
+        primaryQueue = auxiliaryQueue;
+        auxiliaryQueue = temp;
+    }
+    
+    public int pop() {
+        return primaryQueue.pop();
+    }
+    
+    public int top() {
+        return primaryQueue.peek();
+    }
+    
+    public boolean empty() {
+        return primaryQueue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
