58870e1150efd829cc9958e9f3e453e5f331cf67
keaunsol Na <knsol1992@naver.com>
2026-08-14 10:59:31 +0900

    feat: solved 2221-find-triangular-sum-of-an-array

---
+++ leetcode/Solved/Med/2221-find-triangular-sum-of-an-array/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+
+        if (nums.length == 1) return nums[0];
+
+        if (nums.length == 2) return nums[0] + nums[1];
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+
+            int[] arr = new int[nums.length - 2];
+
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = nums[j] + nums[j + 1];
+            }
+
+            nums = arr;
+        }       
+
+        return nums[0];
+    }
+}
