7cd726c7772676cc4dde91b1f73060177d49fdf7
keaunsol Na <knsol1992@naver.com>
2026-08-14 13:31:32 +0900

    feat: solved 0649-dota2-senate

---
+++ leetcode/Solved/Med/0649-dota2-senate/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        
+        char[] arr = senate.toCharArray();
+        List<Character> list = new ArrayList<>();
+
+        for (char c : arr) list.add(c);
+
+        while (list.size() == 1) {
+
+            for (int i = 0; i < list.size(); i++) {
+
+                boolean isRadiant = list.get(i) == 'R';
+
+                for (int j = i + 1; j < list.size(); j++) {
+
+                    if ((isRadiant && list.get(j) == 'D') || (!isRadiant && list.get(j) == 'R'))  {
+                        list.remove(j);
+                    }
+                }
+                
+            }
+        }
+
+        return (list.get(0) == 'R' ? "Radiant" : "Dire");
+    }
+}
