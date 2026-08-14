6a6eebb74d22d730222cf437310223d028c0993a
keaunsol Na <knsol1992@naver.com>
2026-08-14 15:25:25 +0900

    feat: solved 0649-dota2-senate

--- leetcode/Solved/Med/0649-dota2-senate/Solution.java
+++ leetcode/Solved/Med/0649-dota2-senate/Solution.java
@@ -1,39 +1,31 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        
-        char[] arr = senate.toCharArray();
-        List<Character> list = new ArrayList<>();
 
-        for (char c : arr) list.add(c);
-
-        while (list.size() != 1) {
-            
-            System.out.println(list);
-
-            for (int i = 0; i < list.size(); i++) {
-
-                boolean isRadiant = list.get(i) == 'R';
-
-                for (int j = i + 1; j < list.size(); j++) {
-
-                    if (j == list.size() - 1 && list.size() > 2) {
-                        
-                        if ((isRadiant && list.get(j) == 'D') || (!isRadiant && list.get(j) == 'R'))
-                        {
-                            list.remove(0);
-                            break;
-                        }
-                    }
-
-                    else if ((isRadiant && list.get(j) == 'D') || (!isRadiant && list.get(j) == 'R'))  {
-                        list.remove(j);
-                        break;
-                    }
-                }
-                
+        int senateLength = senate.length();
+      
+        Deque<Integer> radiantQueue = new ArrayDeque<>();
+        Deque<Integer> direQueue = new ArrayDeque<>();
+      
+        for (int i = 0; i < senateLength; ++i) {
+            if (senate.charAt(i) == 'R') {
+                radiantQueue.offer(i);
+            } else {
+                direQueue.offer(i);
             }
         }
+      
+        while (!radiantQueue.isEmpty() && !direQueue.isEmpty()) {
+            int radiantIndex = radiantQueue.poll();
+            int direIndex = direQueue.poll();
+          
+            if (radiantIndex < direIndex) {
+                radiantQueue.offer(radiantIndex + senateLength);
+            } else {
+                direQueue.offer(direIndex + senateLength);
+            }
+        }
+      
+        return radiantQueue.isEmpty() ? "Dire" : "Radiant";
 
-        return (list.get(0) == 'R' ? "Radiant" : "Dire");
     }
 }
