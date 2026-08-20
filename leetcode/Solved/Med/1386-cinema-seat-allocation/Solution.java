f4f216f2ed60b7b874ac8a74d3714e02b5a29237
keaunsol Na <knsol1992@naver.com>
2026-08-20 12:49:41 +0900

    feat: solved 1386-cinema-seat-allocation

--- leetcode/Solved/Med/1386-cinema-seat-allocation/Solution.java
+++ leetcode/Solved/Med/1386-cinema-seat-allocation/Solution.java
@@ -1,48 +1,43 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        
+
         boolean[][] seats = new boolean[n][10];
 
-        for (int i = 0; i < reservedSeats.length; i++) {
-
-            seats[reservedSeats[i][0] - 1][reservedSeats[i][1] - 1] = true;
+        for (int[] reserved : reservedSeats) {
+            seats[reserved[0] - 1][reserved[1] - 1] = true;
         }
 
         int ans = 0;
 
-        for (boolean[] i : seats) {
-
-            System.out.print("1234567890");
-            System.out.println();
-            for (boolean j : i) System.out.print(j ? "■" : "□");
-
-            System.out.println();
-        }
-
         for (int i = 0; i < n; i++) {
 
-            for (int j = 0; j < 6; j++) {
+            boolean left =
+                    !seats[i][1] &&
+                    !seats[i][2] &&
+                    !seats[i][3] &&
+                    !seats[i][4];
 
-                if (!seats[i][j] && !seats[i][j + 1] && !seats[i][j + 2] && !seats[i][j + 3]) {
-                    ans++;
-                    seats[i][j] = true;
-                    seats[i][j + 1] = true;
-                    seats[i][j + 2] = true;
-                    seats[i][j + 3] = true;
-                    j += 3;
+            boolean right =
+                    !seats[i][5] &&
+                    !seats[i][6] &&
+                    !seats[i][7] &&
+                    !seats[i][8];
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || right) {
+                ans += 1;
+            } else {
+                boolean middle =
+                        !seats[i][3] &&
+                        !seats[i][4] &&
+                        !seats[i][5] &&
+                        !seats[i][6];
+
+                if (middle) {
+                    ans += 1;
                 }
             }
-        }
-
-        System.out.println("AFTER");
-        
-        for (boolean[] i : seats) {
-
-            System.out.print("1234567890");
-            System.out.println();
-            for (boolean j : i) System.out.print(j ? "■" : "□");
-
-            System.out.println();
         }
 
         return ans;
