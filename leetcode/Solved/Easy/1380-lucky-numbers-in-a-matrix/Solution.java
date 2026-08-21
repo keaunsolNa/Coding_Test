b766100c0461c3c26c7449647423bb954e79f346
keaunsol Na <knsol1992@naver.com>
2026-08-21 15:39:21 +0900

    feat: solved 1380-lucky-numbers-in-a-matrix

--- leetcode/Solved/Easy/1380-lucky-numbers-in-a-matrix/Solution.java
+++ leetcode/Solved/Easy/1380-lucky-numbers-in-a-matrix/Solution.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < matrix.length; i++) {
 
-            int min = 51;
+            int min = Integer.MAX_VALUE;
             int idx = 0;
 
             for (int j = 0; j < matrix[i].length; j++) {
