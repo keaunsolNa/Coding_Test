a454b4efe043d4a5c2d9a5fc008f0e8899ca8608
keaunsol Na <knsol1992@naver.com>
2026-08-21 16:54:08 +0900

    feat: solved 0168-excel-sheet-column-title

--- leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
+++ leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
@@ -10,12 +10,13 @@ class Solution {
         while (true) {
 
             ans.insert(0, alp[columnNumber % 26]);
-            columnNumber /= 26;
 
             if (columnNumber < 26) {
                 // ans.insert(0, alp[columnNumber % 26]);
                 break;
             }
+            
+            columnNumber /= 26;
         }
 
         return ans.toString();
