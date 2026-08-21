058dd1dcc110dbdd03face9f82175a4a43fcc139
keaunsol Na <knsol1992@naver.com>
2026-08-21 17:32:28 +0900

    feat: solved 0404-sum-of-left-leaves

---
+++ leetcode/Solved/Easy/0404-sum-of-left-leaves/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+      
+        int sum = sumOfLeftLeaves(root.right);
+      
+        if (root.left != null) {
+
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+      
+        return sum;
+    }
+}
