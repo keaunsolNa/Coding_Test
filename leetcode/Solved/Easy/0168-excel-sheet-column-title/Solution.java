797fef76415f0f304cca613afb162dd352d5c3da
keaunsol Na <knsol1992@naver.com>
2026-08-21 16:55:31 +0900

    feat: solved 0168-excel-sheet-column-title

--- leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
+++ leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         
         String[] alp = new String[] {"Z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
                                      "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                                     "S", "T", "U", "V", "W", "X", "Y"};
+                                     "S", "T", "U", "V", "W", "X", "Y", "Z"};
        
         StringBuilder ans = new StringBuilder();
         
