2a2c4b511fd5864a63b988fa13e43302451ba5ea
keaunsol Na <knsol1992@naver.com>
2026-08-21 16:51:13 +0900

    feat: solved 0168-excel-sheet-column-title

--- leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
+++ leetcode/Solved/Easy/0168-excel-sheet-column-title/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         
-        String[] alp = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I",
+        String[] alp = new String[] {"", "A", "B", "C", "D", "E", "F", "G", "H", "I",
                                      "J", "K", "L", "M", "N", "O", "P", "Q", "R",
                                      "S", "T", "U", "V", "W", "X", "Y", "Z"};
        
@@ -10,9 +10,9 @@ class Solution {
         while (true) {
 
             ans += alp[columnNumber % 26];
-            columnNumber /= 27;
+            columnNumber /= 26;
 
-            if (columnNumber < 27) break;
+            if (columnNumber < 26) break;
         }
 
         return ans;
