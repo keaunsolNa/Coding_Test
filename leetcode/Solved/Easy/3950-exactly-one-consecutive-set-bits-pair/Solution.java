3af6c3f5cf857593f38d0d5aaa5272d51f09e1b4
keaunsol Na <knsol1992@naver.com>
2026-08-26 16:18:50 +0900

    feat: solved 3950-exactly-one-consecutive-set-bits-pair

---
+++ leetcode/Solved/Easy/3950-exactly-one-consecutive-set-bits-pair/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+        
+        boolean vis = false;
+
+        for (int pre = 0; n > 0; n >>= 1) {
+
+            int cur = n & 1;
+
+            if (pre == cur && cur == 1) {
+                if (vis) return false;
+                vis = true;
+            }
+            
+            pre = cur;
+        }
+        return vis;
+        
+    }
+}
