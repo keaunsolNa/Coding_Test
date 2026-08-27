23f6461a5c9afc7d9cf3c3b5f4a53748dd5a9793
keaunsol Na <knsol1992@naver.com>
2026-08-27 13:20:57 +0900

    feat: solved 3591-check-if-any-element-has-prime-frequency

--- leetcode/Solved/Easy/3591-check-if-any-element-has-prime-frequency/Solution.java
+++ leetcode/Solved/Easy/3591-check-if-any-element-has-prime-frequency/Solution.java
@@ -15,7 +15,7 @@ class Solution {
 
     public boolean isPrime(int n) {
 
-        if (n <= 1) return false;
+        if (n <= 1) return true;
 
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
