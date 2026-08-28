2e9ee8e6829c915d8cddbfdc89e28f55122767c3
keaunsol Na <knsol1992@naver.com>
2026-08-28 16:27:12 +0900

    feat: solved 3114-latest-time-you-can-obtain-after-replacing-characters

--- leetcode/Solved/Easy/3114-latest-time-you-can-obtain-after-replacing-characters/Solution.java
+++ leetcode/Solved/Easy/3114-latest-time-you-can-obtain-after-replacing-characters/Solution.java
@@ -8,11 +8,11 @@ class Solution {
         System.out.println(min);
 
 
-        if (hour.charAt(0) == '?') hour = "1" +  hour.substring(1, 1);
+        if (hour.charAt(0) == '?') hour = "1" +  hour.substring(1, 2);
         if (hour.charAt(1) == '?') hour = hour.substring(0, 1) + (hour.charAt(0) == '0' ? "9" : "1");
 
-        if (min.charAt(0) == '?') min = "5" + min.substring(1, 1);
-        // if (min.charAt(1) == '?') min = min.substring(0, 1) + "9";
+        if (min.charAt(0) == '?') min = "5" + min.substring(1, 2);
+        if (min.charAt(1) == '?') min = min.substring(0, 1) + "9";
 
         return hour + ":" + min;
 
