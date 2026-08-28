da48418f3ae7f8422ba8882832dcd13d41a10397
keaunsol Na <knsol1992@naver.com>
2026-08-28 15:40:56 +0900

    feat: solved 2739-total-distance-traveled

--- leetcode/Solved/Easy/2739-total-distance-traveled/Solution.java
+++ leetcode/Solved/Easy/2739-total-distance-traveled/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int distanceTraveled(int mainTank, int additionalTank) {
 
         int total = 0;
-        while (mainTank != 0) {
+        while (mainTank >= 5) {
             
             total += Math.max(mainTank, 5);
             mainTank = Math.max(0, mainTank - 5);
