758bfac5e4a9067cedf38fa3d9f917b718dab8f8
keaunsol Na <knsol1992@naver.com>
2026-08-28 16:13:49 +0900

    feat: solved 0242-valid-anagram

--- leetcode/Solved/Easy/0242-valid-anagram/Solution.java
+++ leetcode/Solved/Easy/0242-valid-anagram/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        if (s.length() != t.length()) return false;
+        
         Map<Character, Integer> sMap = new HashMap<>();
         Map<Character, Integer> tMap = new HashMap<>();
 
