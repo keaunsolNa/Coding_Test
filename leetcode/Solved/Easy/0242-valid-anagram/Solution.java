c4dbafafeffea07c0e1e28297c9899a7b0c30202
keaunsol Na <knsol1992@naver.com>
2026-08-28 16:04:14 +0900

    feat: solved 0242-valid-anagram

---
+++ leetcode/Solved/Easy/0242-valid-anagram/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) sMap.put(c, sMap.getOrDefault(s, 0) + 1);
+        for (char c : t.toCharArray()) tMap.put(c, tMap.getOrDefault(s, 0) + 1);
+
+        for (Character key : tMap.keySet()) {
+
+            if (tMap.get(key) != sMap.get(key)) return false;
+        }
+
+        return true;
+    }
+}
