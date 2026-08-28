c645c62e75a23e77470e9ae2af5634e0a3047c7f
keaunsol Na <knsol1992@naver.com>
2026-08-28 11:10:55 +0900

    feat: solved 1309-decrypt-string-from-alphabet-to-integer-mapping

---
+++ leetcode/Solved/Easy/1309-decrypt-string-from-alphabet-to-integer-mapping/Solution.java
@@ -0,0 +1,28 @@
+import java.util.StringTokenizer;
+
+class Solution {
+    public String freqAlphabets(String s) {
+        
+        char[] arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            if (arr[i] == '#') {
+                
+                int temp = Integer.parseInt(arr[i - 1] + "" + arr[i - 2]);
+                char c = (char) (temp + 'a');
+                sb.append(c);
+                i -= 2;
+            }
+
+            else {
+                
+                char c = (char) (arr[i] + 'a');
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
