f690f035b25e015d6282e4ec0d921b1648863f9a
keaunsol Na <knsol1992@naver.com>
2026-08-28 15:21:21 +0900

    feat: solved 1700-number-of-students-unable-to-eat-lunch

---
+++ leetcode/Solved/Easy/1700-number-of-students-unable-to-eat-lunch/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+        Stack<Integer> studentStack = new Stack<>();
+        Stack<Integer> sandwichesStack = new Stack<>();
+        
+        for (int i : students) studentStack.push(i);
+        for (int i : sandwiches) sandwichesStack.push(i);
+
+        int ans = 0;
+
+        while (sandwichesStack.isEmpty()) {
+            
+            Stack<Integer> afterStack = new Stack<>();
+            int prevLength = afterStack.size();
+            ans++;
+
+            while (studentStack.isEmpty()) {
+                if (studentStack.peek() == sandwichesStack.peek()) {
+
+                    studentStack.pop();
+                    sandwichesStack.pop();
+                    continue;
+
+                }
+                else 
+                {
+                    afterStack.push(studentStack.pop());
+                }
+            }
+
+            if (prevLength == afterStack.size()) break;
+
+        }
+
+        return ans;
+    }
+}
