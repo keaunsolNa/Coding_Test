bce145c32ffef2a63f9a2e3d1373d4439b00c56f
keaunsol Na <knsol1992@naver.com>
2026-08-28 11:16:04 +0900

    feat: solved 1309-decrypt-string-from-alphabet-to-integer-mapping

--- leetcode/Solved/Easy/1309-decrypt-string-from-alphabet-to-integer-mapping/Solution.java
+++ leetcode/Solved/Easy/1309-decrypt-string-from-alphabet-to-integer-mapping/Solution.java
@@ -10,10 +10,10 @@ class Solution {
 
             if (arr[i] == '#') {
                 
-                int temp = Integer.parseInt(arr[i - 2] + "" + arr[i - 1]);
+                int temp = Integer.parseInt(arr[i - 2] + "" + arr[i - 1]) + 1;
                 System.out.println(temp);
 
-                char c = (char) (temp + '0');
+                char c = (char) (temp + '0' );
                 sb.append(c);
                 i -= 2;
             }
