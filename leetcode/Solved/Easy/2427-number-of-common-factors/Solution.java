c09ad6c34b6a82a7f9731a705e38d1d0052f1010
keaunsol Na <knsol1992@naver.com>
2026-08-28 17:09:47 +0900

    feat: solved 2427-number-of-common-factors

---
+++ leetcode/Solved/Easy/2427-number-of-common-factors/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        
+        int cnt = 0;
+
+        for (int i = 1; i <= Math.min(a, b); i++) {
+
+            if (a % i == 0 && b % i == 0) cnt++;
+        }
+
+        return cnt;
+    }
+}
