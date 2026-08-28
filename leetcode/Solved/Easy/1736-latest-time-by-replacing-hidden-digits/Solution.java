cff4c31dd2ab12ae91b87d08cf72789bc02e58a0
keaunsol Na <knsol1992@naver.com>
2026-08-28 17:36:34 +0900

    feat: solved 1736-latest-time-by-replacing-hidden-digits

---
+++ leetcode/Solved/Easy/1736-latest-time-by-replacing-hidden-digits/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String maximumTime(String time) {
+        
+        String hour = time.split(":")[0];
+        String min = time.split(":")[1];
+
+        if (hour.charAt(0) == '?') hour = "2" + hour.charAt(1);
+        if (hour.charAt(1) == '?') hour = hour.charAt(0) + (hour.charAt(0) == '2' ? "3" : "9");
+        if ((char) hour.charAt(1) - '0' >= 4) hour = "1" + hour.charAt(1);
+
+        if (min.charAt(0) == '?') min = "5" + min.charAt(1);
+        if (min.charAt(1) == '?') min = min.charAt(0) + "9";
+
+        return hour + ":" + min;
+        
+    }
+}
