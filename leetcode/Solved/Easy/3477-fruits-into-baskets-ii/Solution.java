dc4fd86a49923df827be19ff86ba7a13e8749bc0
keaunsol Na <knsol1992@naver.com>
2026-08-31 10:42:05 +0900

    feat: solved 3477-fruits-into-baskets-ii

---
+++ leetcode/Solved/Easy/3477-fruits-into-baskets-ii/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+
+        int cnt = fruits.length;
+
+        for (int i = 0; i < fruits.length; i++) {
+
+            for (int j = 0; j < baskets.length; j++) {
+
+                if (fruits[i] <= baskets[j]) {
+
+                    baskets[j] = 0;
+                    cnt--;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
