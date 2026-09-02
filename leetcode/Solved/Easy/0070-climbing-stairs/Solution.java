3a89e49d853c81cc2816f10173f0287bd8eaa1e8
keaunsol Na <knsol1992@naver.com>
2026-09-02 23:12:08 +0900

    feat: solved 0070-climbing-stairs

---
+++ leetcode/Solved/Easy/0070-climbing-stairs/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int previousTwo = 0;
+        int previousOne = 1;
+      
+        for (int step = 0; step < n; ++step) {
+
+            int current = previousTwo + previousOne;
+          
+            previousTwo = previousOne;
+            previousOne = current;
+        }
+      
+        return previousOne;
+    }
+}
