e1e3ea67247dacea6e3f41bc152ae92b746f573e
keaunsol Na <knsol1992@naver.com>
2026-09-02 23:22:08 +0900

    feat: solved 0003-longest-substring-without-repeating-characters

---
+++ leetcode/Solved/Med/0003-longest-substring-without-repeating-characters/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int[] arr = new int[128];
+      
+        int max = 0;
+        int len = s.length();
+        int left = 0;
+      
+        for (int right = 0; right < len; right++) {
+
+            char cur = s.charAt(right);
+            arr[cur]++;
+          
+            while (arr[cur] > 1) {
+                arr[s.charAt(left)]--;
+                left++;
+            }
+          
+            max = Math.max(max, right - left + 1);
+        }
+      
+        return max;
+    }
+}
