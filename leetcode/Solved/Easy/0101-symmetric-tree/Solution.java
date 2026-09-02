391831d47dc8f9d25b600fc92c195cdd64f30170
keaunsol Na <knsol1992@naver.com>
2026-09-02 23:32:15 +0900

    feat: solved 0101-symmetric-tree

---
+++ leetcode/Solved/Easy/0101-symmetric-tree/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode leftNode, TreeNode rightNode) {
+
+        if (leftNode == rightNode) {
+            return true;
+        }
+      
+        if (leftNode == null || rightNode == null || leftNode.val != rightNode.val) {
+            return false;
+        }
+      
+        return isMirror(leftNode.left, rightNode.right) && 
+               isMirror(leftNode.right, rightNode.left);
+    }
+}
